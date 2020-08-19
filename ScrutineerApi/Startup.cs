3397dbd80d0f0af3e34f1009f5cfb5935c1ed324
Emerson Franks <emersonf@microsoft.com>
2020-08-19 01:08:55 +0000

    Merged PR 3946: Project setup
    - Updated .gitignore
    - remove bin and others
    - add build module
    - merge
    - add build module and cleanup errors so we're starting off clean.

---
+++ ScrutineerApi/Startup.cs
@@ -0,0 +1,84 @@
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using static Turn10.LiveOps.ScrutineerApi.ApplicationSettings;
+
+namespace Turn10.LiveOps.ScrutineerApi
+{
+    /// <summary>
+    ///     Entry point for the app.
+    /// </summary>
+    public sealed class Startup
+    {
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Startup"/> class.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        /// <summary>
+        ///     Configures the services.
+        /// </summary>
+        /// <param name="services">The services.</param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMicrosoftWebApiAuthentication(this.configuration);
+            services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                // Use the groups claim for populating roles
+                options.TokenValidationParameters.RoleClaimType = "roles";
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(AuthorizationPolicies.AssignmentToLiveOpsAdminRoleRequired, policy => policy.RequireRole(AppRole.LiveOpsAdmin));
+                options.AddPolicy(AuthorizationPolicies.AssignmentToLiveOpsAgentRoleRequired, policy => policy.RequireRole(AppRole.LiveOpsAgent));
+            });
+            services.AddControllers();
+            services.AddSwaggerGen();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+
+        /// <summary>
+        ///     Configures the app.
+        /// </summary>
+        /// <param name="applicationBuilder">The application builder.</param>
+        /// <param name="webHostEnvironment">The web host environment.</param>
+        public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment webHostEnvironment)
+        {
+            if (webHostEnvironment.IsDevelopment())
+            {
+                applicationBuilder.UseDeveloperExceptionPage();
+            }
+
+            applicationBuilder.UseAuthentication();
+            applicationBuilder.UseCors("CorsPolicy");
+            applicationBuilder.UseSwagger();
+            applicationBuilder.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            applicationBuilder.UseHttpsRedirection();
+
+            applicationBuilder.UseRouting();
+            applicationBuilder.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
