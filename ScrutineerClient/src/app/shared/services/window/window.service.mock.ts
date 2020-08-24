d5e806ee9b2b66416c7846c3c02a5ac030967d37
Luke Geiken <lugeiken@microsoft.com>
2020-08-24 12:18:12 -0700

    remove copyrights

--- ScrutineerClient/src/app/shared/services/window/window.service.mock.ts
+++ ScrutineerClient/src/app/shared/services/window/window.service.mock.ts
@@ -1,9 +1,3 @@
-// -----------------------------------------------------------------------
-// <copyright company="Microsoft Corporation">
-//   Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>
-// -----------------------------------------------------------------------
-
 import { Injectable } from '@angular/core';
 
 import { WindowService } from './window.service';
