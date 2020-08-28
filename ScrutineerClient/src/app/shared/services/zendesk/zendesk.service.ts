c6f4a46b7d62ab97d8593ddbe8db4a76c1cac6c6
Luke Geiken <lugeiken@microsoft.com>
2020-08-28 12:10:06 -0700

    fix ticket sidebar

--- ScrutineerClient/src/app/shared/services/zendesk/zendesk.service.ts
+++ ScrutineerClient/src/app/shared/services/zendesk/zendesk.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { WindowService } from '@shared/services/window';
-import { Observable, of, throwError } from 'rxjs';
+import { from, Observable } from 'rxjs';
 
 /** Defines the Zendesk Service. */
 @Injectable({
@@ -18,18 +18,18 @@ export class ZendeskService {
     }
 
     /** Gets the zendesk ticket requestor information. */
-    public getTicketRequestor(): Observable<object> {
-        return this.windowService.zafClient().get('ticket.requester');
+    public getTicketRequestor(): Observable<any> {
+        return from(this.windowService.zafClient().get('ticket.requester'));
     }
 
     /** Gets the zendesk ticket fields. */
-    public getTicketFields(): Observable<object> {
-        return this.windowService.zafClient().get('ticketFields');
+    public getTicketFields(): Observable<any> {
+        return from(this.windowService.zafClient().get('ticketFields'));
     }
 
     /** Gets a zendesk custom ticket field. */
-    public getTicketCustomField(field): Observable<object> {
-        return this.windowService.zafClient().get(`ticket.customField:${field}`);
+    public getTicketCustomField(field): Observable<any> {
+        return from(this.windowService.zafClient().get(`ticket.customField:${field}`));
     }
 
     /** Sends https request through zaf client. */
@@ -39,12 +39,12 @@ export class ZendeskService {
 
     /** Gets the current zendesk user. */
     public currentUser(): Observable<any> {
-        return this.windowService.zafClient().get('currentUser');
+        return from(this.windowService.zafClient().get('currentUser'));
     }
 
     /** Gets the zendesk context. */
-    public context(): Observable<object> {
-        return this.windowService.zafClient().context();
+    public context(): Observable<any> {
+        return from(this.windowService.zafClient().context());
     }
 
     /** Resizes the zendesk app. */
