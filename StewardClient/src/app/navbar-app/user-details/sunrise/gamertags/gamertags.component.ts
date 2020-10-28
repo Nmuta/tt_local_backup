8c4bb34f70fb363a82cdf17a0443421863067b03
David Sharer <v-davsha@microsoft.com>
2020-10-27 17:11:41 -0700

    Towards Shared Consoles

--- StewardClient/src/app/navbar-app/user-details/sunrise/gamertags/gamertags.component.ts
+++ StewardClient/src/app/navbar-app/user-details/sunrise/gamertags/gamertags.component.ts
@@ -1,19 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
+/** Retreives and displays related Sunrise accounts by XUID. */
 @Component({
   selector: 'sunrise-gamertags',
   templateUrl: './gamertags.component.html',
   styleUrls: ['./gamertags.component.scss']
 })
-export class GamertagsComponent implements OnInit {
+export class GamertagsComponent implements OnChanges {
   @Input() public xuid?: number;
+
+  /** True while waiting on a request. */
+  public isLoading = true;
+  /** The error received while loading. */
+  public loadError: any;
 
   public data: object[];
 
   constructor() { }
 
   /** Initialization hook. */
-  public ngOnInit(): void {
-  }
+  public ngOnChanges(): void {
+    if (this.xuid === undefined) { return; }
 
+    this.isLoading = true;
+    this.loadError = undefined;
+    this.sunrise.getBanHistoryByXuid(this.xuid)
+      .subscribe(history => {
+        this.isLoading = false;
+        this.history = history;
+        this.banList = this.history.servicesBanHistory.map(servicesBan => {
+          const output: ServicesBanDescription & {correlatedBan: LiveOpsBanDescription} = _.clone(servicesBan) as any;
+          output.correlatedBan = this.correlateLiveOps(servicesBan);
+          return output;
+        })
+      },
+      _error => {
+        this.isLoading = false;
+        this.loadError = _error; // TODO: Display something useful to the user
+      });
+  }
 }
--- StewardClient/src/app/shared/interceptors/fake-api/apis/title/sunrise/player/xuid/sharedConsoleUsers.ts
+++ StewardClient/src/app/shared/interceptors/fake-api/apis/title/sunrise/player/xuid/sharedConsoleUsers.ts
@@ -2,14 +2,14 @@ import { environment } from "@environments/environment";
 import { FakeApiBase } from "@interceptors/fake-api/apis/fake-api-base";
 
 /** Fake API for finding User Flags. */
-export class SunrisePlayerXuidConsoleSharedUsersFakeApi extends FakeApiBase {
+export class SunrisePlayerXuidConsoleSharedConsoleUsersFakeApi extends FakeApiBase {
   /** True when this API is capable of handling the URL. */
   public get canHandle(): boolean {
     const targetingStewardApi = this.request.url.startsWith(environment.stewardApiUrl);
     if (!targetingStewardApi) { return false };
 
     const url = new URL(this.request.url);
-    const regex = /\/?api\/v2\/title\/sunrise\/player\/xuid\((\d+)\)\/console\/sharedUsers\//i;
+    const regex = /\/?api\/v2\/title\/sunrise\/player\/xuid\((\d+)\)\/console\/sharedConsoleUsers\//i;
     return regex.test(url.pathname);
   }
 
