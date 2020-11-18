a4de9c0b0d13e7c4206034d2eb936808470818ec
David Sharer <v-davsha@microsoft.com>
2020-11-18 13:52:27 -0800

    Tests

--- StewardClient/src/app/shared/services/window/window.service.spec.ts
+++ StewardClient/src/app/shared/services/window/window.service.spec.ts
@@ -1,16 +1,28 @@
 ﻿import { TestBed, inject } from '@angular/core/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { WindowService } from './window.service';
 
 describe('service: WindowService', () => {
+  let store: Store;
+  let service: WindowService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([])],
       providers: [WindowService],
     });
+
+    store = TestBed.inject(Store);
+    service = TestBed.inject(WindowService);
   });
 
   it('should be created', inject([WindowService], (service: WindowService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('Actions:', () => {
+    it('OpenWindow should work', () => {
+
+    });
+  });
 });
