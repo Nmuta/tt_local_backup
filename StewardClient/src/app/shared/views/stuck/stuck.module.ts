e2fdaf51135c14610bab53c63ede2ab3eb339d24
David Sharer <v-davsha@microsoft.com>
2020-11-19 11:46:24 -0800

    Open login popup again

--- StewardClient/src/app/shared/views/stuck/stuck.module.ts
+++ StewardClient/src/app/shared/views/stuck/stuck.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StuckComponent } from './stuck.component';
+import { MatCardModule } from '@angular/material/card';
 
 /** Module for stuck warning component. */
 @NgModule({
   declarations: [StuckComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   exports: [StuckComponent]
 })
 export class StuckModule { }
