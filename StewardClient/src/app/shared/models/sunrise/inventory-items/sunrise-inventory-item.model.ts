c7a8f18bb7deff2c2601631f9d4f1cb8f9edc496
Luke Geiken <lugeiken@microsoft.com>
2020-11-24 09:43:28 -0800

    added sunrise inventory model and associated item models

---
+++ StewardClient/src/app/shared/models/sunrise/inventory-items/sunrise-inventory-item.model.ts
@@ -0,0 +1,9 @@
+/** Interface for sunrise player inventory item. */
+export interface SunriseInventoryItem {
+  itemId?: number;
+  quantity?: unknown;
+  acquisitionUtc?: unknown;
+  modifiedUtc?: unknown;
+  lastUsedUtc?: unknown;
+  description?: string;
+}
