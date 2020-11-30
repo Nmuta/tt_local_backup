4e2a9da0ae024e66d579fb77ace81a9a7ea0cd0c
David Sharer <v-davsha@microsoft.com>
2020-11-30 10:57:28 -0800

    Fix tests; cleanup

--- StewardClient/src/app/sidebars/settings/settings.component.spec.ts
+++ StewardClient/src/app/sidebars/settings/settings.component.spec.ts
@@ -2,17 +2,18 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxsModule } from '@ngxs/store';
 import { of } from 'rxjs';
+import { UserSettingsState } from '@shared/state/user-settings/user-settings.state';
 
 import { SettingsComponent } from './settings.component';
 import { UserSettingsStateModel } from '@shared/state/user-settings/user-settings.state';
 
-describe('SettingsComponent', () => {
+fdescribe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NgxsModule.forRoot([])],
+      imports: [NgxsModule.forRoot([UserSettingsState])],
       declarations: [SettingsComponent],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
