17ee8d60b6dc402840235a9da0665bad2f70639e
David Sharer <v-davsha@microsoft.com>
2021-01-03 17:53:53 -0800

    Ticket App Drawer

--- StewardClient/src/app/pages/ticket-app/ticket-app.component.ts
+++ StewardClient/src/app/pages/ticket-app/ticket-app.component.ts
@@ -1,6 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BaseComponent } from '@components/base-component/base-component.component';
 import { Select } from '@ngxs/store';
+import { WindowService } from '@services/window';
 import { UserModel } from '@shared/models/user.model';
 import { ZendeskService } from '@shared/services/zendesk';
 import { UserState } from '@shared/state/user/user.state';
@@ -18,7 +20,12 @@ export class TicketAppComponent extends BaseComponent implements OnInit, AfterVi
   public loading: boolean;
   public profile: UserModel;
 
-  constructor(private readonly zendesk: ZendeskService) {
+  constructor(
+    private readonly zendesk: ZendeskService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly windowService: WindowService,
+  ) {
     super();
   }
 
@@ -41,5 +48,19 @@ export class TicketAppComponent extends BaseComponent implements OnInit, AfterVi
   /** Logic for the AfterViewInit component lifecycle. */
   public ngAfterViewInit(): void {
     this.zendesk.resize$('100%', '500px').subscribe();
+  }
+
+  /** Clears the current sidebar outlet path. */
+  public clearSidebar(): void {
+    // https://github.com/angular/angular/issues/5122
+    this.router.navigate([{ outlets: { sidebar: null } }], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
+  }
+
+  /** Produces the current location, for reference when in iframe. */
+  public get location(): string {
+    return `${this.windowService.location().pathname}${this.windowService.location().search}`;
   }
 }
--- StewardClient/src/app/pages/ticket-app/ticket-app.routing.ts
+++ StewardClient/src/app/pages/ticket-app/ticket-app.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FourOhFourComponent } from '@shared/views/four-oh-four/four-oh-four.component';
 import { AuthGuard } from 'app/route-guards/auth.guard';
+import { sidebarRoutes } from 'app/sidebars/sidebars.module';
 import { ApolloComponent } from './pages/apollo/apollo.component';
 import { GravityComponent } from './pages/gravity/gravity.component';
 import { OpusComponent } from './pages/opus/opus.component';
@@ -38,7 +39,7 @@ const routes: Routes = [
         path: 'sunrise',
         component: SunriseComponent,
       },
-      // TODO: Sidebar?
+      ...sidebarRoutes,
       {
         path: '**',
         component: FourOhFourComponent,
