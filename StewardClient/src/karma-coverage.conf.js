62d7793fa78fcae02affebd705248d9975fb7039
David Sharer <v-davsha@microsoft.com>
2021-01-08 13:40:42 -0800

    Update to karma-coverage

--- StewardClient/src/karma-coverage.conf.js
+++ StewardClient/src/karma-coverage.conf.js
@@ -15,23 +15,46 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      reports: ['html', 'cobertura', 'text', 'lcov'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      // see https://github.com/karma-runner/karma-coverage/blob/master/docs/configuration.md
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'cobertura' },
+        { type: 'lcov' },
+      ],
+      check: {
         global: {
           statements: 60,
           lines: 60,
           functions: 60,
+          excludes: [
+            '**/*.spec.ts',
+            'src/shared/interceptors/fake-api/**/*',
+          ]
         },
         each: {
           statements: 20,
           lines: 20,
           functions: 20,
+          excludes: [
+            '**/*.spec.ts',
+          ]
         },
-      },
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
