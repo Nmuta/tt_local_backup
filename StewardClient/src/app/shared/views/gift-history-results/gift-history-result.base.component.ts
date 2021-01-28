7ba4051df80874c3c5f2d51c65a381395d2b81c7
Luke Geiken <lugeiken@microsoft.com>
2021-01-28 13:28:23 -0800

    adding cancel request observable to ngOnChanges so if player content is remove from the page mid-request, the observable is cancelled accordingly

--- StewardClient/src/app/shared/views/gift-history-results/gift-history-result.base.component.ts
+++ StewardClient/src/app/shared/views/gift-history-results/gift-history-result.base.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { BaseComponent } from '@components/base-component/base-component.component';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { IdentityResultAlpha, IdentityResultBeta } from '@models/identity-query.model';
 import { LspGroup } from '@models/lsp-group';
-import { NEVER, Observable } from 'rxjs';
+import { NEVER, Observable, Subject } from 'rxjs';
 import { GravityGiftHistories } from '@models/gravity';
 import { SunriseGiftHistories } from '@models/sunrise';
 import { ApolloGiftHistories } from '@models/apollo';
@@ -18,10 +18,12 @@ type GiftHistoryResultUnion = GravityGiftHistories | SunriseGiftHistories | Apol
   })
 export abstract class GiftHistoryResultsBaseComponent<T extends IdentityResultUnion, U extends GiftHistoryResultUnion>
   extends BaseComponent
-  implements OnChanges, OnInit {
+  implements OnChanges {
   @Input() public selectedPlayer: T;
   @Input() public selectedGroup: LspGroup;
   @Input() public usingPlayerIdentities: boolean;
+
+  public cancelGiftHistoryRequest$: Observable<void>;
 
   /** The error received while loading. */
   public loadError: unknown;
@@ -29,7 +31,6 @@ export abstract class GiftHistoryResultsBaseComponent<T extends IdentityResultUn
   public isLoading = false;
   /** The gift history list to display. */
   public giftHistoryList: U;
-  public giftHistoryList$: Observable<U>;
 
   public isActiveIcon = faCheck;
   
@@ -40,25 +41,31 @@ export abstract class GiftHistoryResultsBaseComponent<T extends IdentityResultUn
   public abstract retrieveHistoryByPlayer(): Observable<U>;
   public abstract retrieveHistoryByLspGroup(): Observable<U>;
 
-    public ngOnInit(): void {  
+  /** Angular lifecycle hook. */
+  public ngOnChanges(_changes: SimpleChanges): void {
+    // Verify component data is valid before sending off request
+    if((this.usingPlayerIdentities && !this.selectedPlayer) || (!this.usingPlayerIdentities && !this.selectedGroup)) {
+      // Cancel the previous observable is the user clears out the existing lookup information
+      (this.cancelGiftHistoryRequest$ as Subject<void>)?.next();
+      (this.cancelGiftHistoryRequest$ as Subject<void>)?.complete();
+      this.giftHistoryList = null;
+      return;
     }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        this.isLoading = true;
-        const getGiftHistory$ = this.usingPlayerIdentities ? this.retrieveHistoryByPlayer() : this.retrieveHistoryByLspGroup();
+    this.isLoading = true;
+    this.cancelGiftHistoryRequest$ = new Subject<void>();
 
-        this.giftHistoryList$ = getGiftHistory$.pipe(
-            takeUntil(this.onDestroy$),
-            catchError(error => {
-                this.isLoading=false;
-                this.loadError = error;
-                return NEVER;
-            }),
-            tap(giftHistories => {
-                console.log("in the tap");
-                this.isLoading = false;
-                this.giftHistoryList = giftHistories;
-            })
-        )
-    }
+    const getGiftHistory$ = this.usingPlayerIdentities ? this.retrieveHistoryByPlayer() : this.retrieveHistoryByLspGroup();
+    getGiftHistory$.pipe(
+      takeUntil(this.onDestroy$ || this.cancelGiftHistoryRequest$),
+      catchError(error => {
+          this.isLoading = false;
+          this.loadError = error;
+          return NEVER;
+      }),
+      tap(giftHistories => {
+          this.isLoading = false;
+          this.giftHistoryList = giftHistories;
+      })).subscribe();
+  }
 }
