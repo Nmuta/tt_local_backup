cbb72e67d3cf5aa3bee27d591232ba33dbaf31df
David Sharer <v-davsha@microsoft.com>
2021-02-02 15:02:53 -0800

    inventory base model typings

--- StewardClient/src/app/shared/models/extended-types.ts
+++ StewardClient/src/app/shared/models/extended-types.ts
@@ -1,2 +1,8 @@
 /** For strings that look like guids. */
 export type GuidLikeString = string;
+
+/** For Xuids that are strings. */
+export type XuidString = string;
+
+/** Turn 10 IDs can be either a xuid or a guid-like-string. */
+export type Turn10IdString = XuidString | GuidLikeString;
--- StewardClient/src/app/shared/models/gravity/gravity-player-inventory.model.ts
+++ StewardClient/src/app/shared/models/gravity/gravity-player-inventory.model.ts
@@ -1,11 +1,12 @@
+import { GuidLikeString, Turn10IdString } from '@models/extended-types';
 import { GravityCar, GravityInventoryItem, GravityKit } from './inventory-items';
 
 /** Interface for gravity player inventory. */
 export interface GravityPlayerInventory {
   xuid: BigInt;
-  t10Id: string;
-  previousGameSettingsId: string;
-  currentExternalProfileId: unknown;
+  t10Id: Turn10IdString;
+  previousGameSettingsId: GuidLikeString;
+  currentExternalProfileId: GuidLikeString;
   cars: GravityCar[];
   masteryKits: GravityInventoryItem[];
   upgradeKits: GravityKit[];
