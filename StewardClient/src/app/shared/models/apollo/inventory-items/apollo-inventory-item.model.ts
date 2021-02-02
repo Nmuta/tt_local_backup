d269d203c283ad988fcf31e078a2030a985063d6
David Sharer <v-davsha@microsoft.com>
2021-02-02 14:49:04 -0800

    Inventory Items typings

--- StewardClient/src/app/shared/models/apollo/inventory-items/apollo-inventory-item.model.ts
+++ StewardClient/src/app/shared/models/apollo/inventory-items/apollo-inventory-item.model.ts
@@ -2,9 +2,9 @@
 export interface ApolloInventoryItem {
   itemId: BigInt;
   quantity: BigInt;
-  acquisitionUtc: unknown;
-
-  lastUsedUtc: unknown;
+  acquisitionUtc: Date;
+  modifiedUtc: never; // apollo does not have this value
+  lastUsedUtc: Date;
   description: string;
-  special: string;
+  special: 'Unicorn' | ''; // may be other values
 }
--- StewardClient/src/app/shared/models/sunrise/inventory-items/sunrise-inventory-item.model.ts
+++ StewardClient/src/app/shared/models/sunrise/inventory-items/sunrise-inventory-item.model.ts
@@ -1,9 +1,9 @@
 /** Interface for sunrise player inventory item. */
 export interface SunriseInventoryItem {
   itemId: BigInt;
-  quantity: unknown;
-  acquisitionUtc: unknown;
-  modifiedUtc: unknown;
-  lastUsedUtc: unknown;
+  quantity: BigInt;
+  acquisitionUtc: Date;
+  modifiedUtc: Date;
+  lastUsedUtc: Date;
   description: string;
 }
