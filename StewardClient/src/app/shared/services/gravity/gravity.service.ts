24bed021dd2f9c315372d0027d36d9abb124aaaa
David Sharer <v-davsha@microsoft.com>
2021-02-02 16:45:06 -0800

    cleanup gravity

--- StewardClient/src/app/shared/services/gravity/gravity.service.ts
+++ StewardClient/src/app/shared/services/gravity/gravity.service.ts
@@ -57,19 +57,6 @@ export class GravityService {
       );
   }
 
-  /** Gets gravity player details with a XUID. */
-  public getPlayerDetailsByXuid(xuid: string): Observable<GravityPlayerDetails> {
-    return this.apiService
-      .getRequest<GravityPlayerDetails>(`${this.basePath}/player/xuid(${xuid})/details`)
-      .pipe(
-        map(details => {
-          details.firstLoginUtc = !!details.firstLoginUtc ? new Date(details.firstLoginUtc) : null;
-          details.lastLoginUtc = !!details.lastLoginUtc ? new Date(details.lastLoginUtc) : null;
-          return details;
-        }),
-      );
-  }
-
   /** Gets gravity player details with a T10 ID. */
   public getPlayerDetailsByT10Id(t10Id: string): Observable<GravityPlayerDetails> {
     return this.apiService
@@ -89,64 +76,12 @@ export class GravityService {
   }
 
   /** Gets gravity player inventory with a profile ID. */
-  public getPlayerInventoryByProfileIdWithXuid(
-    xuid: string,
-    profileId: string,
-  ): Observable<GravityPlayerInventory> {
-    return this.apiService.getRequest<GravityPlayerInventory>(
-      `${this.basePath}/player/inventory/xuid(${xuid})/profileId(${profileId})`,
-    );
-  }
-
-  /** Gets gravity player inventory with a profile ID. */
   public getPlayerInventoryByProfileIdWithT10Id(
     t10Id: string,
     profileId: string,
   ): Observable<GravityPlayerInventory> {
     return this.apiService.getRequest<GravityPlayerInventory>(
-      `${this.basePath}/player/inventory/t10Id(${t10Id})/profileId(${profileId})`,
-    );
-  }
-
-  /** Updates gravity player inventory with a XUID. */
-  public updatePlayerInventoryByXuid(
-    inventory: GravityPlayerInventory,
-    useBackgroundProcessing: boolean = false,
-  ): Observable<GravityPlayerInventory> {
-    if (!inventory.xuid) {
-      return throwError('No XUID provided.');
-    }
-
-    const params = new HttpParams().append(
-      'useBackgroundProcessing',
-      useBackgroundProcessing.toString(),
-    );
-
-    return this.apiService.postRequest<GravityPlayerInventory>(
-      `${this.basePath}/player/inventory/xuid`,
-      inventory,
-      params,
-    );
-  }
-
-  /** Updates gravity player inventory with a T10 Id. */
-  public updatePlayerInventoryByT10Id(
-    inventory: GravityPlayerInventory,
-    useBackgroundProcessing: boolean = false,
-  ): Observable<GravityPlayerInventory> {
-    if (!inventory.t10Id || inventory.t10Id === '') {
-      return throwError('No T10 Id provided.');
-    }
-
-    const params = new HttpParams().append(
-      'useBackgroundProcessing',
-      useBackgroundProcessing.toString(),
-    );
-
-    return this.apiService.postRequest<GravityPlayerInventory>(
-      `${this.basePath}/player/inventory/t10Id`,
-      inventory,
-      params,
+      `${this.basePath}/player/t10Id(${t10Id})/profileId(${profileId})/inventory`,
     );
   }
 
