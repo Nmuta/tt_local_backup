0d05bb48ec90345ff884f2ea1c962bae5c03d98d
David Sharer <v-davsha@microsoft.com>
2021-02-03 14:58:54 -0800

    Fake BigInt, Fake Inventory APIs

--- StewardClient/src/app/shared/interceptors/fake-api/apis/title/opus/player/xuid/inventory.ts
+++ StewardClient/src/app/shared/interceptors/fake-api/apis/title/opus/player/xuid/inventory.ts
@@ -1,7 +1,7 @@
 import { environment } from '@environments/environment';
 import { FakeApiBase } from '@interceptors/fake-api/apis/fake-api-base';
+import { fakeBigInt, faker } from '@interceptors/fake-api/utility';
 import { OpusPlayerInventory } from '@models/opus';
-import { Unprocessed } from '@models/unprocessed';
 
 /** Fake API for opus player inventory. */
 export class OpusPlayerXuidInventoryFakeApi extends FakeApiBase {
@@ -22,12 +22,30 @@ export class OpusPlayerXuidInventoryFakeApi extends FakeApiBase {
   }
 
   /** Produces a sample API response. */
-  public handle(): Partial<Unprocessed<OpusPlayerInventory>> {
+  public handle(): OpusPlayerInventory {
     return OpusPlayerXuidInventoryFakeApi.make();
   }
 
   /** Generates a sample object */
-  public static make(): Partial<Unprocessed<OpusPlayerInventory>> {
-    return {};
+  public static make(): OpusPlayerInventory {
+    return {
+      credits: fakeBigInt({ min: 0 }),
+      cars: Array(faker.random.number(200)).fill(0).map(() => {
+        return {
+          vin: faker.random.uuid(),
+          baseCost: fakeBigInt({ min: 4_000 }),
+          collectorScore: fakeBigInt({ min: 4_000, max: 200_000}),
+          isOnlineOnly: faker.random.boolean(),
+          productionNumber: fakeBigInt({ min: 4_000, max: 200_000}),
+          purchaseUtc: faker.date.past(),
+          versionedLiveryId: faker.random.uuid(),
+          versionedTuneId: faker.random.uuid(),
+          carId: fakeBigInt(),
+          dateCreatedUtc: faker.date.past(2 /*years*/),
+          displayName: faker.lorem.words(3),
+          special: faker.random.arrayElement(['Unicorn', '']),
+        };
+      }),
+    };
   }
 }
--- StewardClient/src/app/shared/interceptors/fake-api/utility/index.ts
+++ StewardClient/src/app/shared/interceptors/fake-api/utility/index.ts
@@ -1,5 +1,6 @@
 import * as realFaker from 'faker';
 export * from './fake-xuid';
+export * from './fake-bigint';
 
 /** Faker */
 export const faker = realFaker; // just so imports will work properly.
