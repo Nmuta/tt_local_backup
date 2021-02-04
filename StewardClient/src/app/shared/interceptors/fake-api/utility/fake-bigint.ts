ded312c26c485c024783907afe14d95ae3eaa5d8
David Sharer <v-davsha@microsoft.com>
2021-02-04 12:41:55 -0800

    fix tests

--- StewardClient/src/app/shared/interceptors/fake-api/utility/fake-bigint.ts
+++ StewardClient/src/app/shared/interceptors/fake-api/utility/fake-bigint.ts
@@ -8,7 +8,7 @@ interface FakeBigIntParams {
 /** Generate a fake BigInt that is at least as large as MAX_SAFE_INTEGER by default. */
 export function fakeBigInt(params: FakeBigIntParams = { min: Number.MAX_SAFE_INTEGER }): bigint {
   const min = BigInt(params.min ?? 0);
-  const max = BigInt(params.max ?? undefined)
+  const max = params.max ? BigInt(params.max) : undefined;
 
   let result = min;
 
