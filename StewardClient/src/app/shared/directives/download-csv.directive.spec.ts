e39ce707b5640119b94674add5e48db0a435de55
Jordan Yates <v-joyate@microsoft.com>
2021-02-22 15:23:38 -0800

    prettier fix

--- StewardClient/src/app/shared/directives/download-csv.directive.spec.ts
+++ StewardClient/src/app/shared/directives/download-csv.directive.spec.ts
@@ -12,7 +12,9 @@ describe('DownloadCsvDirective', () => {
     beforeEach(() => {
       anchorEl = document.createElement('a') as HTMLAnchorElement;
       anchorEl.setAttribute = jasmine.createSpy('setAttribute');
-      anchorEl.click = jasmine.createSpy('click').and.callFake(() => { /** Empty */});
+      anchorEl.click = jasmine.createSpy('click').and.callFake(() => {
+        /** Empty */
+      });
       anchorEl.remove = jasmine.createSpy('remove');
       spyOn(document, 'createElement').and.returnValue(anchorEl);
       spyOn(document.body, 'appendChild').and.callThrough();
@@ -20,7 +22,10 @@ describe('DownloadCsvDirective', () => {
 
     it('should create an anchor tag to download the data and remove the tag after', () => {
       const directive = new DownloadCsvDirective();
-      directive.downloadCsv = [ ['col1', 'col2'], [ 'row1val1', 'row1val2' ]];
+      directive.downloadCsv = [
+        ['col1', 'col2'],
+        ['row1val1', 'row1val2'],
+      ];
       const event = new Event('click');
       directive.onClick(event);
 
