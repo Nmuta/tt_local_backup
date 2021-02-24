06d21403c3efb3c6efae183411fc2ec6e40de13d
Luke Geiken <lugeiken@microsoft.com>
2021-02-24 14:31:26 -0800

    not working state

---
+++ StewardApi/Contracts/Exceptions/StewardInventoryItemError.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+using Turn10.Data.Common;
+
+namespace Turn10.LiveOps.StewardApi.Contracts.Exceptions
+{
+    /// <summary>
+    ///     Represents an error with an inventory item.
+    /// </summary>`
+    public sealed class StewardInventoryItemError
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="StewardInventoryItemError"/> class.
+        /// </summary>
+        public StewardInventoryItemError()
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="StewardInventoryItemError"/> class.
+        /// </summary>
+        /// <param name="item">The message.</param>
+        public StewardInventoryItemError(MasterInventoryItem item)
+        {
+            item.ShouldNotBeNull(nameof(item));
+
+            this.Item = item;
+        }
+
+        /// <summary>
+        ///     Gets or sets the item.
+        /// </summary>
+        public MasterInventoryItem Item { get; set; }
+    }
+}
---
+++ StewardApi/Logging/ILoggingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Turn10.LiveOps.StewardApi.Logging
+{
+    /// <summary>
+    ///     Exposes methods for logging to application insights.
+    /// </summary>
+    public interface ILoggingService
+    {
+        /// <summary>
+        ///     Logs an exception to application insights.
+        /// </summary>
+        /// <param name="eventName">The event name.</param>
+        /// <param name="eventProperties">The event properties.</param>
+        void LogCustomEvent(string eventName, IDictionary<string, string> eventProperties);
+
+        /// <summary>
+        ///     Logs an exception to application insights.
+        /// </summary>
+        /// <param name="ex">The exception to log.</param>
+        void LogException(Exception ex);
+    }
+}
