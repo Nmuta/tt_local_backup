6a29b1bed898715e3744a1a4282803973012a975
Luke Geiken <lugeiken@microsoft.com>
2021-02-26 10:53:12 -0800

    retry auth only once and allow 3 second timeout before rerequesting

--- StewardClient/src/app/shared/interceptors/access-token.interceptor.ts
+++ StewardClient/src/app/shared/interceptors/access-token.interceptor.ts
@@ -1,16 +1,18 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Store } from '@ngxs/store';
 import { LoggerService, LogTopic } from '@services/logger';
 import { RecheckAuth } from '@shared/state/user/user.actions';
 import { UserState } from '@shared/state/user/user.state';
-import { Observable } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
+import { NEVER, Observable } from 'rxjs';
+import { catchError, switchMap, tap, timeout } from 'rxjs/operators';
 
 /** Defines the access token interceptor. */
 @Injectable()
 export class AccessTokenInterceptor implements HttpInterceptor {
+  public tokenRetryParam: string = 'tokenRetry';
+
   constructor(private readonly store: Store, private readonly logger: LoggerService) {}
 
   /** Intercept logic that adds bearer token to request header. */
@@ -23,23 +25,47 @@ export class AccessTokenInterceptor implements HttpInterceptor {
       return next.handle(request);
     }
 
+    request = this.setAccessTokenHeader(request);
+
+    return next.handle(request).pipe(
+      catchError((error) => {
+        if (this.isAuthError(error) && !this.hasAlreadyRetried(request)) {
+          return this.store.dispatch(new RecheckAuth()).pipe(
+            timeout(3_000),
+            switchMap(() => {
+              request = this.setAccessTokenHeader(request);
+              request = request.clone({
+                setParams: {
+                  [this.tokenRetryParam]: 'true',
+                },
+              });
+              return next.handle(request);
+            })
+          );
+        }
+
+        throw error;
+      }),
+    );
+  }
+
+  private isAuthError(error: unknown): boolean {
+    return error instanceof HttpErrorResponse && error.status === 401;
+  }
+
+  private hasAlreadyRetried(request: HttpRequest<unknown>): boolean {
+    const retried = request.params.get(this.tokenRetryParam);
+    return !!retried;
+  }
+
+  private setAccessTokenHeader(request: HttpRequest<unknown>): HttpRequest<unknown> {
     let accessToken = this.store.selectSnapshot<string | null | undefined>(UserState.accessToken);
     accessToken = !!accessToken ? accessToken : '';
-    request = request.clone({
+
+    return request.clone({
       setHeaders: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-
-    return next.handle(request).pipe(
-      catchError((error, caught) => {
-        if (error.status === 401) {
-          this.logger.warn([LogTopic.Auth], 'Received 401; attempting silent reauth and retry');
-          return this.store.dispatch(new RecheckAuth()).pipe(switchMap(() => caught));
-        }
-
-        return caught;
-      }),
-    );
   }
 }
