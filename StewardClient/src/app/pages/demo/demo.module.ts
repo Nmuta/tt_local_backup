7d85411f2dfe0a21047abc630634f3b6c487909b
David Sharer <v-davsha@microsoft.com>
2021-03-03 14:00:02 -0800

    Font Icons and Demonstration

---
+++ StewardClient/src/app/pages/demo/demo.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoComponent } from './demo.component';
+import { DemoRouterModule } from './demo.routing';
+import { MatCardModule } from '@angular/material/card';
+import { CenterContentsModule } from '@components/center-contents/center-contents.module';
+
+/** Demonstration and style testing pages. */
+@NgModule({
+  declarations: [DemoComponent],
+  imports: [
+    DemoRouterModule,
+    CommonModule,
+    MatCardModule,
+    CenterContentsModule,
+  ]
+})
+export class DemoModule { }
