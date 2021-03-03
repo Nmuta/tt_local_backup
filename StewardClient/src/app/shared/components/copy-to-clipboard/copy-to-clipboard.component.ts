ffa3793e7635078082fc895e7e7bab60fd042c0d
Jordan Yates <v-joyate@microsoft.com>
2021-03-03 14:29:33 -0800

    work

--- StewardClient/src/app/shared/components/copy-to-clipboard/copy-to-clipboard.component.ts
+++ StewardClient/src/app/shared/components/copy-to-clipboard/copy-to-clipboard.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-copy-to-clipboard',
   templateUrl: './copy-to-clipboard.component.html',
   styleUrls: ['./copy-to-clipboard.component.scss']
 })
-export class CopyToClipboardComponent implements OnInit {
+export class CopyToClipboardComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public readonly copyIcon = faCopy;
 }
