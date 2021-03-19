5940844766c7d3103ca57f66e23a521c76c4115a
Luke Geiken <luke.geiken@microsoft.com>
2021-03-19 17:12:56 +0000

    Merged PR 7130: Steward UI: Hide fake api toggle from production unless user is a LiveOpsAdmin
    Fake API data looks too real and can be confusing if not recognized

--- StewardClient/src/app/sidebars/settings/settings.component.spec.ts
+++ StewardClient/src/app/sidebars/settings/settings.component.spec.ts
@@ -1,15 +1,19 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { of } from 'rxjs';
 import { UserSettingsState } from '@shared/state/user-settings/user-settings.state';
 
 import { SettingsComponent } from './settings.component';
 import { UserSettingsStateModel } from '@shared/state/user-settings/user-settings.state';
+import { UserModel } from '@models/user.model';
+import { UserRole } from '@models/enums';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
   let fixture: ComponentFixture<SettingsComponent>;
+
+  let mockStore: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +28,13 @@ describe('SettingsComponent', () => {
     component.settings$ = of(<UserSettingsStateModel>{
       enableFakeApi: false,
     });
+
+    mockStore = TestBed.inject(Store);
+    mockStore.selectSnapshot = jasmine.createSpy('selectSnapshot').and.returnValue({
+      emailAddress: 'fake-email',
+      name: 'fake-name',
+      role: UserRole.LiveOpsAdmin,
+    } as UserModel);
   });
 
   beforeEach(() => {
--- StewardClient/src/app/sidebars/settings/settings.component.ts
+++ StewardClient/src/app/sidebars/settings/settings.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent } from '@components/base-component/base-component.component';
+import { environment } from '@environments/environment';
+import { UserRole } from '@models/enums';
+import { UserModel } from '@models/user.model';
 import { Select, Store } from '@ngxs/store';
 import { SetFakeApi } from '@shared/state/user-settings/user-settings.actions';
 import {
   UserSettingsState,
   UserSettingsStateModel,
 } from '@shared/state/user-settings/user-settings.state';
+import { UserState } from '@shared/state/user/user.state';
 import { Observable } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
@@ -18,6 +22,7 @@ export class SettingsComponent extends BaseComponent implements OnInit {
   @Select(UserSettingsState) public settings$: Observable<UserSettingsStateModel>;
 
   public enableFakeApi: boolean;
+  public showFakeApiToggle: boolean; // Only show on dev or if user is a live ops admin
 
   constructor(private readonly store: Store) {
     super();
@@ -25,6 +30,9 @@ export class SettingsComponent extends BaseComponent implements OnInit {
 
   /** Initialization hook. */
   public ngOnInit(): void {
+    const profile = this.store.selectSnapshot<UserModel>(UserState.profile);
+    this.showFakeApiToggle = profile.role === UserRole.LiveOpsAdmin || !environment.production;
+
     this.settings$
       .pipe(takeUntil(this.onDestroy$))
       .subscribe(latest => (this.enableFakeApi = latest.enableFakeApi));
