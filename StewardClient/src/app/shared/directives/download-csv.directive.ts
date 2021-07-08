53ed313e47f7b879916dbeda950e96155b060a01
Luke Geiken <luke.geiken@microsoft.com>
2021-07-08 22:18:50 +0000

    Merged PR 9098: Steward UI: JSON table download reformats numbers so Excel no longer cuts off digits past length of 15.

--- StewardClient/src/app/shared/directives/download-csv.directive.ts
+++ StewardClient/src/app/shared/directives/download-csv.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, HostListener, Input } from '@angular/core';
+import BigNumber from 'bignumber.js';
 
 /** Downloads the CSV input to the client browser. */
 @Directive({
@@ -13,7 +14,26 @@ export class DownloadCsvDirective {
   @HostListener('click', ['$event'])
   public onClick(_event: Event): void {
     const csvContent =
-      'data:text/csv;charset=utf-8,' + this.downloadCsv.map(row => row.join(',')).join('\n');
+      'data:text/csv;charset=utf-8,' +
+      this.downloadCsv
+        .map(row => {
+          return row
+            .map(entry => {
+              // Excel has a floating point limitation of 15 characters.
+              // For BigNumber and strings that are numbers, we need to use format "=""${data}"""
+              // so that characters at length 16+ are not replaced with zeros.
+              if (
+                BigNumber.isBigNumber(entry) ||
+                (typeof entry === 'string' && !new BigNumber(entry).isNaN())
+              ) {
+                return `"=""${entry}"""`;
+              }
+
+              return entry;
+            })
+            .join(',');
+        })
+        .join('\n');
 
     const encodedUri = encodeURI(csvContent);
     const fileName = `${this.filname}_${new Date().toISOString()}.csv`;
