b6e8d2020c14e4fbf2a6a5d053feea3a2e0eb075
David Sharer <v-davsha@microsoft.com>
2021-12-21 00:54:28 +0000

    Merged PR 12087: Make home page glassy and add backgrounds

--- StewardClient/src/app/pages/home/home.component.ts
+++ StewardClient/src/app/pages/home/home.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { DateTime } from 'luxon';
 
 /** Displays the home page splash page. */
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public centerContentsClasses = {};
+  constructor() {
+    const dayOfWeek = DateTime.local().weekday - 1;
+    const weekOfYear = DateTime.local().weekNumber;
+    const whichBackground = ((weekOfYear + dayOfWeek) % 7) + 1;
+    this.centerContentsClasses = { [`bg${whichBackground}`]: true };
+  }
+}
