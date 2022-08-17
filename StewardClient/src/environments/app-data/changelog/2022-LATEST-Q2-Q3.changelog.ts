66c42d21d0ba71e7ae006ccec19d80fe0a93ea0d
David Sharer <sharerdavid@microsoft.com>
2022-08-17 16:55:23 -0700

    Allow gifting certain time-limited liveries as part of inventory restoration

--- StewardClient/src/environments/app-data/changelog/2022-LATEST-Q2-Q3.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2022-LATEST-Q2-Q3.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2022_Q2_Q3: ChangelogGroup = {
   id: '8d8286fe-2fd7-421a-bae8-607212cac0e2',
   entries: [
     {
+      tag: { title: GameTitle.FH5, tool: NavbarTool.Gifting },
+      uuid: '2b7a9b68-9150-4604-ba16-c4b175d40320',
+      shortText: 'Allow gifting certain time-limited liveries as part of inventory restoration',
+    },
+    {
       tag: { title: GameTitle.FH5, tool: NavbarTool.Leaderboards },
       uuid: '91b5cae4-d232-40dd-8b65-689a09541524',
       shortText: 'Add leaderboard talent gridlines to graph view',
