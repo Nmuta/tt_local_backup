4479bcbbdeb713956b357850acdf540f848f8241
David Sharer <sharerdavid@microsoft.com>
2022-08-26 12:10:39 -0700

    flip to Q4 changelog

--- StewardClient/src/environments/app-data/changelog/changelog.ts
+++ StewardClient/src/environments/app-data/changelog/changelog.ts
@@ -1,8 +1,9 @@
 import { CHANGELOG_2022_Q2_Q3 } from './2022-LATEST-Q2-Q3.changelog';
+import { CHANGELOG_2022_Q4 } from './2022-Q4.changelog';
 import { Changelog } from './types';
 
 /** All loaded changelogs. */
 export const CHANGELOG: Changelog = {
-  active: [CHANGELOG_2022_Q2_Q3],
+  active: [CHANGELOG_2022_Q4, CHANGELOG_2022_Q2_Q3],
   inactive: [],
 };
