e1e10398e804f65f14bd2424e6750f712b85c22a
David Sharer <sharerdavid@microsoft.com>
2022-08-26 12:14:31 -0700

    Rename files

--- StewardClient/src/environments/app-data/changelog/changelog.ts
+++ StewardClient/src/environments/app-data/changelog/changelog.ts
@@ -1,5 +1,5 @@
-import { CHANGELOG_2022_Q2_Q3 } from './2022-LATEST-Q2-Q3.changelog';
-import { CHANGELOG_2022_Q4 } from './2022-Q4.changelog';
+import { CHANGELOG_2022_Q2_Q3 } from './2022-Q2-Q3.changelog';
+import { CHANGELOG_2022_Q4 } from './2022-LATEST-Q4.changelog';
 import { Changelog } from './types';
 
 /** All loaded changelogs. */
