bb7e94a3c6111b7fcba3e683b469d644f0a75646
David Sharer <sharerdavid@microsoft.com>
2022-08-26 13:27:20 -0700

    Standard Copy Module

---
+++ StewardClient/src/app/shared/modules/standard-copy/standard-copy-icon/standard-copy-icon.component.spec.ts
@@ -0,0 +1,37 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { StandardCopyIconComponent } from './standard-copy-icon.component';
+
+@Component({
+  template: '<standard-copy>test</standard-copy>',
+})
+class TestHarnessComponent {}
+
+describe('StandardCopyIconComponent', () => {
+  let component: StandardCopyIconComponent;
+  let fixture: ComponentFixture<StandardCopyIconComponent>;
+  // let harnessComponent: TestHarnessComponent;
+  let harness: ComponentFixture<TestHarnessComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestHarnessComponent, StandardCopyIconComponent],
+      imports: [MatTooltipModule],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(StandardCopyIconComponent);
+    component = fixture.componentInstance;
+    harness = TestBed.createComponent(TestHarnessComponent);
+    // harnessComponent = harness.componentInstance;
+    harness.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
