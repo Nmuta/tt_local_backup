ec342b1893f81309fc23037ba1e7eb9edafb09d7
Jordan Yates <v-jyates@microsoft.com>
2022-09-19 14:18:57 -0700

    cleanup and such

--- StewardClient/src/environments/app-data/changelog/2022-LATEST-Q4.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2022-LATEST-Q4.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2022_Q4: ChangelogGroup = {
   id: 'cd4c3f29-ea08-4550-aec2-de051686419a',
   entries: [
     {
+      tag: { title: GameTitle.FM8, tool: NavbarTool.Messaging },
+      uuid: '5d7bad3d-5f99-4f6f-a77f-c08d579c75d3',
+      shortText: 'Implemented localized message creation and sending',
+    },
+    {
       tag: { title: [GameTitle.FH5, GameTitle.FM8], tool: NavbarTool.UserDetails },
       uuid: 'feb2c9f8-0bf4-486a-9c9a-e29d5a070da0',
       shortText: 'Unlock Content Creator user flag',
