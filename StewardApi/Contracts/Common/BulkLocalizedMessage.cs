d920679dc73bac8b509031aaa1529bc28b131512
Jordan Yates <v-jyates@microsoft.com>
2022-09-20 10:37:36 -0700

    bad formatting

--- StewardApi/Contracts/Common/BulkLocalizedMessage.cs
+++ StewardApi/Contracts/Common/BulkLocalizedMessage.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 
 #pragma warning disable SA1600 // ElementsMustBeDocumented (POCO mapped directly from LSP)
 #pragma warning disable CS1591 // XML Comments (POCO mapped directly from LSP)
-
-/// <summary>
-///     Represents a bulk localized message.
-/// </summary>
 namespace Turn10.LiveOps.StewardApi.Contracts.Common
 {
+    /// <summary>
+    ///     Represents a bulk localized message.
+    /// </summary>
     public class BulkLocalizedMessage : LocalizedMessage
     {
         public IList<ulong> Xuids { get; set; }
