f8ea8d8d370505a1dfa8c49e7191f19da5d12c7d
Ellen Porter (SHE/HER) <elporter@microsoft.com>
2022-10-10 14:07:11 -0700

    Restore Program.cs

--- StewardApi/Program.cs
+++ StewardApi/Program.cs
@@ -1,13 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.ActionConstraints;
-using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Turn10.LiveOps.StewardApi.Authorization;
 
 namespace Turn10.LiveOps.StewardApi
 {
@@ -21,28 +13,7 @@ namespace Turn10.LiveOps.StewardApi
         /// </summary>
         public static void Main(string[] arguments)
         {
-            var host = CreateHostBuilder(arguments).Build();
-
-            /*
-            var collection = host.Services.GetService(typeof(IActionDescriptorCollectionProvider)) as ActionDescriptorCollectionProvider;
-            List<string> lines = new List<string>();
-            foreach (var descriptor in collection.ActionDescriptors.Items)
-            {
-                var cad = descriptor as ControllerActionDescriptor;
-                var api = cad.AttributeRouteInfo.Template;
-                var controllerName = cad.ControllerName;
-
-                var rolesOnAction = cad.MethodInfo.GetCustomAttributes(typeof(AuthorizeRolesAttribute), true).Select(attr => (attr as AuthorizeRolesAttribute).Roles);
-                var rolesOnController = cad.ControllerTypeInfo.GetCustomAttributes(typeof(AuthorizeRolesAttribute), true).Select(attr => (attr as AuthorizeRolesAttribute).Roles);
-
-                var method = cad.ActionConstraints.Where(c => c is HttpMethodActionConstraint).FirstOrDefault() as HttpMethodActionConstraint;
-                lines.Add($"{controllerName}:{cad.ActionName}:{string.Join(",", method.HttpMethods)}:{api}:{string.Join(",", rolesOnAction.Concat(rolesOnController))}");
-            }
-            
-            File.WriteAllLinesAsync("C:\\Users\\elporter\\Desktop\\Steward.txt", lines);
-            */
-
-            host.Run();
+            CreateHostBuilder(arguments).Build().Run();
         }
 
         /// <summary>
