6785621ed102c11d4c59ea88bdd9db7f817ea77d
David Sharer <sharerdavid@microsoft.com>
2022-10-11 17:20:22 -0700

    Tests

--- StewardClient/src/app/shared/modules/endpoints/endpoints-nav-tool/endpoints-nav-tool.component.spec.ts
+++ StewardClient/src/app/shared/modules/endpoints/endpoints-nav-tool/endpoints-nav-tool.component.spec.ts
@@ -1,4 +1,7 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppIcon, CommonAccessLevels, ExtraIcon, HomeTileInfoCustomTile, NavbarTool } from '@environments/environment';
+import { NgxsModule } from '@ngxs/store';
 
 import { EndpointsNavToolComponent } from './endpoints-nav-tool.component';
 
@@ -8,13 +11,28 @@ describe('EndpointsNavToolComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([])],
       declarations: [EndpointsNavToolComponent],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EndpointsNavToolComponent);
     component = fixture.componentInstance;
+    component = fixture.componentInstance;
+    component.item = <HomeTileInfoCustomTile>{
+      icon: AppIcon.DeveloperTool,
+      extraIcon: ExtraIcon.External,
+      tool: NavbarTool.AdminPagesSelector,
+      accessList: CommonAccessLevels.AdminPageAccess,
+      title: 'Admin Pages',
+      subtitle: 'Production / Flight / Dev',
+      imageUrl: undefined,
+      imageAlt: undefined,
+      tooltipDescription: 'Various Admin Pages',
+      shortDescription: [`Various Admin Pages`],
+    };
     fixture.detectChanges();
   });
 
