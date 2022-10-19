4645fb664ba04cb8d055ea5073984d6b6eb1528f
Jordan Yates <v-jyates@microsoft.com>
2022-10-19 22:31:39 +0000

    Merged PR 17290: Modify layout of FH5 user details

--- StewardClient/src/environments/app-data/changelog/2022-LATEST-Q4.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2022-LATEST-Q4.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2022_Q4: ChangelogGroup = {
   id: 'cd4c3f29-ea08-4550-aec2-de051686419a',
   entries: [
     {
+      tag: { title: GameTitle.FH5, tool: NavbarTool.UserDetails },
+      uuid: '2e847b23-bc1d-4b1b-8b72-765146235af0',
+      shortText: 'Modify layout of FH5 user details to place smaller tools across the top',
+    },
+    {
       tag: { title: [GameTitle.FM8, GameTitle.FH5, GameTitle.FH4], tool: [NavbarTool.UgcDetails] },
       uuid: '898997d1-c8fb-41c0-b435-2f889b3e340b',
       shortText: 'Modified layout of UGC actions in UGC Details view',
