bcb12d031578c5ab65ee23d47583430f027f6ff5
Jordan Yates <v-jyates@microsoft.com>
2022-12-19 21:59:25 +0000

    Merged PR 18749: Correlate ban history for FH5 with ban entry ID after December 1st 2022
    Bans now start when a player first logs in, not at the time of ban submission. This caused desyncs in FH5 ban history because we correlated our entries with services by comparing start/expire times.
    
    This fix uses ban entry ID as source of truth for newer bans, and uses the old method for any bans started before Dec 1st 2022.
    
    before:
    ![image (2).png](https://turn10.visualstudio.com/4043f02f-acc8-4003-8330-9187c43a2ab0/_apis/git/repositories/99f68dc4-7f8d-4b91-9173-efd898e50254/pullRequests/18749/attachments/image%20%282%29.png)
    after:
    ![image.png](https://turn10.visualstudio.com/4043f02f-acc8-4003-8330-9187c43a2ab0/_apis/git/repositories/99f68dc4-7f8d-4b91-9173-efd898e50254/pullRequests/18749/attachments/image.png)

--- StewardApi/Helpers/BanHistoryConsolidationHelper.cs
+++ StewardApi/Helpers/BanHistoryConsolidationHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
 using Turn10.LiveOps.StewardApi.Contracts.Data;
 using Turn10.LiveOps.StewardApi.Contracts.Exceptions;
 using Turn10.LiveOps.StewardApi.Logging;
@@ -17,8 +18,13 @@ namespace Turn10.LiveOps.StewardApi.Helpers
         /// </summary>
         public static List<LiveOpsBanHistory> ConsolidateBanHistory(IList<LiveOpsBanHistory> liveOpsBanHistory, IList<LiveOpsBanHistory> servicesBanHistory, ILoggingService loggingService, string gameTitle)
         {
+            IEqualityComparer<LiveOpsBanHistory> titleAppropriateBanHistoryComparer =
+                gameTitle == TitleCodeName.Woodstock.ToString()
+                    ? new LiveOpsBanHistoryIdComparer()
+                    : new LiveOpsBanHistoryComparer();
+
             return liveOpsBanHistory.Concat(servicesBanHistory)
-                                                .GroupBy(history => history, new LiveOpsBanHistoryComparer())
+                                                .GroupBy(history => history, titleAppropriateBanHistoryComparer)
                                                 .Select(banGroups => ConsolidateBanHistory(banGroups, loggingService, gameTitle))
                                                 .Where(entry => entry != null)
                                                 .ToList();
