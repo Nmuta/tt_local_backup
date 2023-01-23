bda9800c797cfbc5fb82417461f3ac35bcdbe275
Luke Geiken <luke.geiken@microsoft.com>
2023-01-23 22:08:20 +0000

    Merged PR 19213: Add ability to sync AAD user list to Cosmos DB user list.
    Handles both new users and users with updated roles.
    
    ![image.png](https://dev.azure.com/turn10/4043f02f-acc8-4003-8330-9187c43a2ab0/_apis/git/repositories/99f68dc4-7f8d-4b91-9173-efd898e50254/pullRequests/19213/attachments/image.png)

--- StewardClient/src/app/pages/tools/pages/permission-management/permission-management.component.ts
+++ StewardClient/src/app/pages/tools/pages/permission-management/permission-management.component.ts
@@ -17,6 +17,7 @@ import {
   PermissionAttributeList,
   PermissionsService,
 } from '@services/api-v2/permissions/permissions.service';
+import { V2UsersService } from '@services/api-v2/users/users.service';
 import { PermAttribute, PermAttributeName } from '@services/perm-attributes/perm-attributes';
 import { UserService } from '@services/user';
 import { ActionMonitor } from '@shared/modules/monitor-action/action-monitor';
@@ -93,6 +94,7 @@ export class PermissionManagementComponent extends BaseComponent implements OnIn
   public selectUserToManage$ = new Subject<UserModelWithPermissions>();
   public nameFilterFormControl = new FormControl(null);
 
+  public postSyncUsersDbActionMonitor = new ActionMonitor('POST sync users DB');
   public getPermissionsActionMonitor = new ActionMonitor('GET permision attributes');
   public titleEnvironments: TitleEnvironments = {
     [GameTitle.Forte]: [],
@@ -134,6 +136,7 @@ export class PermissionManagementComponent extends BaseComponent implements OnIn
   constructor(
     private readonly dialog: MatDialog,
     private readonly userService: UserService,
+    private readonly v2UsersService: V2UsersService,
     private readonly permissionsService: PermissionsService,
   ) {
     super();
@@ -294,7 +297,19 @@ export class PermissionManagementComponent extends BaseComponent implements OnIn
   /** Whether the node has a child */
   public hasChild = (_: number, _nodeData: AttributeTreeFlatNode) => _nodeData.expandable;
 
+  /** Syncs the user DB. */
+  public syncUsersDb(): void {
+    this.postSyncUsersDbActionMonitor = this.postSyncUsersDbActionMonitor.repeat();
+    this.v2UsersService
+      .syncDb$()
+      .pipe(this.postSyncUsersDbActionMonitor.monitorSingleFire(), takeUntil(this.onDestroy$))
+      .subscribe(() => {
+        this.initStewardUsersList();
+      });
+  }
+
   private initStewardUsersList(): void {
+    this.getUsersActionMonitor = this.getUsersActionMonitor.repeat();
     this.userService
       .getAllStewardUsers$()
       .pipe(this.getUsersActionMonitor.monitorSingleFire(), takeUntil(this.onDestroy$))
