8d536d2223077458619c514545d1a72a3c19e5b1
Luke Geiken <lugeiken@microsoft.com>
2023-01-25 14:24:06 -0800

    add changelog entry

--- StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
@@ -8,9 +8,14 @@ export const CHANGELOG_2023_Q1: ChangelogGroup = {
   id: '056f7a46-ee96-4dbf-a689-f3edbef2c34d',
   entries: [
     {
+      tag: ChangelogTag.General,
+      uuid: '7dde5027-cadc-4d90-a809-5c8dca247831',
+      shortText: 'Add tool & route restrictions for auth V2',
+    },
+    {
       tag: ChangelogTag.Internal,
       uuid: '89e244fe-d239-44fe-b6e6-8470e5beb278',
-      shortText: 'Cut changelog. Updated to 2023 Q1.',
+      shortText: 'Cut changelog. Updated to 2023 Q1',
     },
   ],
 };
