03fe6268e272138498e34d87ba80c3e7db80b9e5
Luke Geiken <luke.geiken@microsoft.com>
2023-02-23 23:54:45 +0000

    Merged PR 20248: Update contact us dev list

--- StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2023_Q1: ChangelogGroup = {
   id: '056f7a46-ee96-4dbf-a689-f3edbef2c34d',
   entries: [
     {
+      tag: ChangelogTag.Internal,
+      uuid: '28348e1d-a311-4f49-be39-9e7663898d5a',
+      shortText: 'Update contact us dev list',
+    },
+    {
       tag: { title: [GameTitle.FH5], tool: NavbarTool.CreateAuction },
       uuid: 'f27c15dc-f9de-4667-b4b8-859f7a13a643',
       shortText: 'Fix permission error',
