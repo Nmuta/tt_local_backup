55816cee9d394c4293a21cb148579cac9ec087c5
Luke Geiken <lugeiken@microsoft.com>
2023-03-07 21:00:48 -0800

    Show error when valid ban reason is not selected

--- StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2023_Q1: ChangelogGroup = {
   id: '056f7a46-ee96-4dbf-a689-f3edbef2c34d',
   entries: [
     {
+      tag: { title: 'all', tool: NavbarTool.UserBanning },
+      uuid: '7880276c-1f28-4927-8f80-7fbc92acf04a',
+      shortText: 'Show error when valid ban reason is not selected',
+    },
+    {
       tag: { title: [GameTitle.FH5, GameTitle.FM8], tool: NavbarTool.UgcDetails },
       uuid: '947b953a-d168-432b-a743-9e449ba30441',
       shortText: 'Searching now includes both featured and non-featured UGC content',
