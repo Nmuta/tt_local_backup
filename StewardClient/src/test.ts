e6bb648e96eca6ad44f3356ba77ff6bc5ba9d931
Luke Geiken <lugeiken@microsoft.com>
2023-04-04 11:45:52 -0700

    clean up test modules after each test

--- StewardClient/src/test.ts
+++ StewardClient/src/test.ts
@@ -12,7 +12,7 @@ declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
-  teardown: { destroyAfterEach: false },
+  teardown: { destroyAfterEach: true },
 });
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
