39c1ff33f97ff53bb9221f460c444e310f216a74
Sebastian <v-scushman@microsoft.com>
2023-05-19 08:08:36 -0700

    Removed No-Longer Relevant Comment

--- StewardClient/cypress/support/steward/shared-functions/verify-chip.ts
+++ StewardClient/cypress/support/steward/shared-functions/verify-chip.ts
@@ -1,7 +1,6 @@
 /** Verifies a chip exists after player lookup. */
 export function verifyChip(chipText: string): void {
   it('should have valid chip', () => {
-    // found bans
     cy.contains('mat-chip', chipText, { matchCase: false });
   });
 }
