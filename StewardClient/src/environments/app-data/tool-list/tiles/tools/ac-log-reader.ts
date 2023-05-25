3f90eb66cd3a83c79da174b19a9b509e5d5e5b3c
Jordan Yates <jordan.yates@turn10.msgamestudios.com>
2023-05-25 14:27:39 -0700

    PR feedback

--- StewardClient/src/environments/app-data/tool-list/tiles/tools/ac-log-reader.ts
+++ StewardClient/src/environments/app-data/tool-list/tiles/tools/ac-log-reader.ts
@@ -4,14 +4,16 @@ import { HomeTileInfoInternal, AppIcon, NavbarTool } from '../../helpers';
 export const acLogReaderTile = <HomeTileInfoInternal>{
   icon: AppIcon.AcLogReader,
   tool: NavbarTool.AcLogReader,
-  title: 'AC Log Reader',
-  subtitle: 'Decode client logs',
+  title: 'Anti-Cheat Log Reader',
+  subtitle: 'Decode client crash logs to find evidence of cheating',
   supportedTitles: [GameTitle.FM8],
   allPermissions: [],
   imageUrl: undefined,
   imageAlt: undefined,
-  tooltipDescription: 'Decode gameplay logs.',
-  shortDescription: ['Tool for decoding client logs.'],
+  tooltipDescription: 'Decode gameplay crash logs.',
+  shortDescription: [
+    'Tool for decoding client logs (.Crash_Info files) to find evidence of cheating.',
+  ],
   loadChildren: () =>
     import('../../../../../app/pages/tools/pages/ac-log-reader/ac-log-reader.module').then(
       m => m.AcLogReaderModule,
