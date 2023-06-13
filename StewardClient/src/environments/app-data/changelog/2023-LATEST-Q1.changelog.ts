7b9bf909fa4c8413fe2144db2e8a2021e987e252
Terrence Donnelly <v-tdonnelly@microsoft.com>
2023-06-13 12:56:15 -0500

    Show share code in tooltip when hovering copy button after clone

--- StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2023_Q1: ChangelogGroup = {
   id: '056f7a46-ee96-4dbf-a689-f3edbef2c34d',
   entries: [
     {
+      tag: { title: [GameTitle.FH5], tool: NavbarTool.UgcDetails },
+      uuid: '92489393-b7da-42a0-be5b-0f006f3bdf58',
+      shortText: 'Show Share Code when hovering copy button after clone UGC',
+    },
+    {
       tag: { title: [GameTitle.FM8], tool: NavbarTool.ShowroomCalendar },
       uuid: 'e33f5cf3-a63e-4683-9c69-532e5a64b6eb',
       shortText: 'Add division and manufacturer featured showcases',
