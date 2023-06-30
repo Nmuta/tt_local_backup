3a001f0118ed022473571de46350508452c3f3bb
Jordan Yates <jordan.yates@turn10.msgamestudios.com>
2023-06-29 17:19:19 -0700

    UI made purty, waiting on response from Stephen/Tyler for final popover text content.

--- StewardClient/src/app/pages/tools/pages/services-table-storage/components/services-filterable-table/services-filterable-table.component.ts
+++ StewardClient/src/app/pages/tools/pages/services-table-storage/components/services-filterable-table/services-filterable-table.component.ts
@@ -46,7 +46,7 @@ export class ServicesFilterableTableComponent extends BaseComponent implements O
   public filteredCategories: Observable<string[]>;
 
   public formControls = {
-    filterResults: new FormControl(true),
+    showAll: new FormControl(false),
   };
 
   public formGroup: FormGroup = new FormGroup(this.formControls);
@@ -77,7 +77,7 @@ export class ServicesFilterableTableComponent extends BaseComponent implements O
 
     this.getMonitor = this.getMonitor.repeat();
     this.contract
-      .getTableStorageByProfileId$(this.contract?.xuid, this.contract?.externalProfileId, this.formControls.filterResults.value)
+      .getTableStorageByProfileId$(this.contract?.xuid, this.contract?.externalProfileId, !this.formControls.showAll.value)
       .pipe(this.getMonitor.monitorSingleFire(), takeUntil(this.onDestroy$))
       .subscribe(results => {
         const data = results;
