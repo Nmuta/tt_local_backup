53511f0839401780ca1c4112c4e2bd13455e034b
Sebastian <v-scushman@microsoft.com>
2023-06-30 09:55:21 -0700

    Cypress UGC Details Steelhead Tests

--- StewardClient/cypress/integration/steward-tools/ugc-details/shared-functions.ts
+++ StewardClient/cypress/integration/steward-tools/ugc-details/shared-functions.ts
@@ -1,5 +1,6 @@
 import { waitForProgressSpinners } from '@support/steward/common/wait-for-progress-spinners';
 
+//** Fills in Livery ugcid and confirms population */
 export function testLivery(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
@@ -18,6 +19,7 @@ export function testLivery(ugcid, createdDate): void {
     .should('exist');
 }
 
+//** Fills in Layer Group ugcid and confirms population */
 export function testLayerGroup(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
@@ -36,6 +38,7 @@ export function testLayerGroup(ugcid, createdDate): void {
     .should('exist');
 }
 
+//** Fills in Photo ugcid and confirms population */
 export function testPhoto(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
@@ -54,6 +57,7 @@ export function testPhoto(ugcid, createdDate): void {
     .should('exist');
 }
 
+//** Fills in Tune ugcid and confirms population */
 export function testTune(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
@@ -72,6 +76,7 @@ export function testTune(ugcid, createdDate): void {
     .should('exist');
 }
 
+//** Fills in Event ugcid and confirms population */
 export function testEvents(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
@@ -90,7 +95,27 @@ export function testEvents(ugcid, createdDate): void {
     .should('exist');
 }
 
+//** Fills in Community Challenge ugcid and confirms population */
 export function testCommunityChallenge(ugcid, createdDate): void {
+  cy.get('mat-form-field')
+    .contains('mat-label', 'UGC ID / Share Code')
+    .parent()
+    .parent()
+    .parent()
+    .click()
+    .type(ugcid);
+  waitForProgressSpinners();
+
+  cy.get('model-dump-simple-table').contains('div', ugcid).should('exist');
+  cy.get('tr')
+    .contains('th', 'Created Date Utc')
+    .parent()
+    .contains('div', createdDate)
+    .should('exist');
+}
+
+//** Fills in Tune Blob ugcid and confirms population */
+export function testTuneBlob(ugcid, createdDate): void {
   cy.get('mat-form-field')
     .contains('mat-label', 'UGC ID / Share Code')
     .parent()
