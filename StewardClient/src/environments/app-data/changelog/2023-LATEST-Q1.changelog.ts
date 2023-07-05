1f99f5afff4ae542be510fca64216cbdf9af0843
Terrence Donnelly <v-tdonnelly@microsoft.com>
2023-07-05 15:42:07 -0500

    Update changelog

--- StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
+++ StewardClient/src/environments/app-data/changelog/2023-LATEST-Q1.changelog.ts
@@ -8,6 +8,12 @@ export const CHANGELOG_2023_Q1: ChangelogGroup = {
   id: '056f7a46-ee96-4dbf-a689-f3edbef2c34d',
   entries: [
     {
+      tag: { title: [GameTitle.FH5], tool: NavbarTool.SearchUGC },
+      uuid: '7bbd5f84-b9ec-4854-b1fc-e1982659d389',
+      shortText:
+        'Implement bulk UGC reporting in UGC Search',
+    },
+    {
       tag: { title: [GameTitle.FH5], tool: NavbarTool.CreateAuction },
       uuid: 'c1eac190-9ab8-4b19-9f39-796a62271153',
       shortText: 'Ensure valid car is selected in create single auction',
