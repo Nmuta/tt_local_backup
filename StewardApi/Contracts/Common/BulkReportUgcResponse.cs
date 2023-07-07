639a387c241053a72f81a4b03a4a5b6fa73e9f7f
Terrence Donnelly <v-tdonnelly@microsoft.com>
2023-07-07 12:40:49 -0500

    Fix comment

--- StewardApi/Contracts/Common/BulkReportUgcResponse.cs
+++ StewardApi/Contracts/Common/BulkReportUgcResponse.cs
@@ -9,12 +9,12 @@ namespace Turn10.LiveOps.StewardApi.Contracts.Common
     public class BulkReportUgcResponse
     {
         /// <summary>
-        ///     Gets or sets the ugcId.
+        ///     Gets or sets the UgcId.
         /// </summary>
         public Guid UgcId { get; set; }
 
         /// <summary>
-        ///     Gets or sets the generate sharecode error.
+        ///     Gets or sets the UGC report error.
         /// </summary>
         public StewardError? Error { get; set; }
     }
