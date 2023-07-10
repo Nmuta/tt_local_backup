efa5f46a05979a08b06a3ea0101c9d2069732c35
William Henning <v-whenning@microsoft.com>
2023-07-10 13:32:23 -0700

    Added test for the auction details pages for woodstock and sunrise.

---
+++ StewardClient/cypress/integration/steward-tools/auction-details/sunrise.spec.ts
@@ -0,0 +1,31 @@
+import { login } from '@support/steward/auth/login';
+import { disableFakeApi } from '@support/steward/util/disable-fake-api';
+import { goToTool } from './page';
+import { selectSunrise} from '@support/steward/shared-functions/game-nav';
+import { verifyAuctionDeleteButtonUnlock, verifyAuctionInformation, verifyInvalidAuctionSearch } from './shared-tests';
+
+// The following is an auction ID belonging to the dev account chada AKA r2dubs
+// TODO: Bug should perhaps be filed if necessary, as this same ID works in both sunrise and woodstock but likely originates from woodstock.
+const validAuctionId = '91aed181-952c-48ed-a61a-ec4933a95902'
+
+context('Steward / Tools / Auction Details / Sunrise', () => {
+  beforeEach(() => {
+    login();
+    disableFakeApi();
+  });
+
+  context('Auction Lookup', () => {
+    beforeEach(() => {
+      goToTool();
+      selectSunrise();
+    });
+
+    //TODO: No test account has interacted with the auctions in Sunrise on the prod server. 07/10
+    //This test still ostensibly works, but may not be intended behavior.
+    verifyAuctionInformation(validAuctionId);
+    verifyInvalidAuctionSearch();
+
+    //TODO: currently Sunrise seems to have no delete button for liveries. 07/10
+    //verifyAuctionDeleteButtonUnlock(validAuctionId);
+  });
+});
