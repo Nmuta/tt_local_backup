2b1ee85b1339ed50be11549598053443d67bdd9a
David Sharer <sharerdavid@microsoft.com>
2023-07-14 14:37:26 -0700

    Migrate all deployables to /packages; Split Cypress into its own package; Update Cypress; Update Node

---
+++ packages/StewardClient-Cypress/cypress/configs/localhost.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  env: {
+    STEWARD_API: 'https://steward-api-dev.azurewebsites.net',
+    TEST_WOODSTOCK: true,
+    TEST_STEELHEAD: true,
+    AAD_APP_ENV: 'DEV',
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    experimentalRunAllSpecs: true,
+    baseUrl: 'http://localhost:4200',
+    specPattern: 'cypress/e2e/**/*.spec.*',
+    supportFile: 'cypress/support/commands.ts',
+  },
+})
---
+++ packages/StewardClient-Cypress/cypress/configs/prod.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  chromeWebSecurity: false,
+  env: {
+    STEWARD_API: 'https://steward-api-prod.azurewebsites.net',
+    TEST_WOODSTOCK: false,
+    TEST_STEELHEAD: false,
+    AAD_APP_ENV: 'PROD',
+  },
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  video: false,
+  screenshotOnRunFailure: false,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  e2e: {
+    experimentalRunAllSpecs: true,
+    baseUrl: 'https://steward-ui-prod.azurewebsites.net',
+    specPattern: 'cypress/e2e/**/*.spec.*',
+    supportFile: 'cypress/support/commands.ts',
+  },
+})
