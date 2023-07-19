1dbc96b6a485d1e09644206c9a6d5f1bc5db155e
David Sharer <sharerdavid@microsoft.com>
2023-07-19 15:17:35 -0700

    Move files

---
+++ packages/StewardApi/StewardApi/Models/TestResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Turn10.LiveOps.StewardApi.Models
+{
+    /// <summary>
+    ///     A test response.
+    /// </summary>
+    public sealed class TestResponse
+    {
+        /// <summary>
+        ///     Gets or sets a value indicating whether the request was successful.
+        /// </summary>
+        public bool Result { get; set; }
+    }
+}
