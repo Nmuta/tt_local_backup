63f2a48e55dfb0214724e6917aae05332e10180e
Sebastian <v-scushman@microsoft.com>
2023-07-20 16:04:27 -0700

    Removed T10ID Tests

--- packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/services-table-storage/steelhead.spec.ts
+++ packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/services-table-storage/steelhead.spec.ts
@@ -1,7 +1,7 @@
 import { login } from "@support/steward/auth/login";
 import { stewardUrls } from "@support/steward/urls";
 import { disableFakeApi } from "@support/steward/util/disable-fake-api";
-import { searchByGtag, searchByT10Id, searchByXuid } from '@support/steward/shared-functions/searching';
+import { searchByGtag, searchByXuid } from '@support/steward/shared-functions/searching';
 import { StudioUsers } from "@support/steward/common/account-info";
 import { steelheadResults, testInvalidSearches, testLookup, testLookupWithFilter, testLookupWithInvalidFilter, testPlayerProfilesPopulated, testShowAllRows } from "./shared-functions";
 import { changeEndpoint } from "@support/steward/shared-functions/change-endpoint";
@@ -40,17 +40,4 @@ context('Steward / Tools / Services Table Storage / Steelhead', () => {
     testShowAllRows(steelheadResults.showAllRows);
     testLookupWithInvalidFilter();
   });
-
-  // Takes an extremely long time, and the field to use T10 ID is "T10 ID" when the searchByT10Id function looks for "T10 Id"
-  // context('T10 ID Tests', () => {
-  //   beforeEach(() => {
-  //     searchByT10Id(StudioUsers['caleb'].t10Id);
-  //   });
-
-  //   testPlayerProfilesPopulated();
-  //   testLookup(steelheadResults.noFilter);
-  //   testLookupWithFilter(steelheadResults.CMSOverrideFilter);
-  //   testShowAllRows(steelheadResults.showAllRows);
-  //   testLookupWithInvalidFilter();
-  // });
 });
