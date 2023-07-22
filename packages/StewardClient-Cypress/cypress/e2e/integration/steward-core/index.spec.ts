8dfe30b44c2d0b06f26dd565361c3b002fe9daf2
David Sharer <sharerdavid@microsoft.com>
2023-07-21 18:30:57 -0700

    Multiple reporters; JUnit reporting; Merged JUnit Reporting

--- packages/StewardClient-Cypress/cypress/e2e/integration/steward-core/index.spec.ts
+++ packages/StewardClient-Cypress/cypress/e2e/integration/steward-core/index.spec.ts
@@ -21,7 +21,7 @@ context('Steward Index', () => {
     cy.visit('/');
   });
 
-  it('should lead to Tools homepage', () => {
+  it.only('should lead to Tools homepage', () => {
     // Verfiy cards
     cy.get('.mat-card-title').contains('Player Details');
     cy.get('.mat-card-title').contains('UGC Search');
