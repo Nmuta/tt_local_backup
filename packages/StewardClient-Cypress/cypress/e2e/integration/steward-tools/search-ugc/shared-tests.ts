a28d0dd97dccc922fe28648a27d735d01e3e6b01
William Henning <v-whenning@microsoft.com>
2023-07-25 13:38:09 -0700

    Lint tests

--- packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/search-ugc/shared-tests.ts
+++ packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/search-ugc/shared-tests.ts
@@ -8,18 +8,18 @@ export function selectUgcType(ugcType: string): void {
 }
 
 export function selectCar(carModel: string): void {
-  cy.get('make-model-autocomplete').within(()=>{
-    cy.get('input').click({ force: true }).type(carModel, {force: true});
+  cy.get('make-model-autocomplete').within(() => {
+    cy.get('input').click({ force: true }).type(carModel, { force: true });
   });
   cy.get('.cdk-overlay-container').contains(carModel).click();
 }
 
 export function orderUgcBy(order: string): void {
   cy.contains('mat-form-field', 'Order By').click();
-      cy.get('.cdk-overlay-container').contains('mat-option', order).click();
+  cy.get('.cdk-overlay-container').contains('mat-option', order).click();
 }
 
-export function selectCuratedUgcType(curatedUgcType: string): void{
+export function selectCuratedUgcType(curatedUgcType: string): void {
   cy.contains('mat-form-field', 'UGC Curated Type').click();
   cy.get('.cdk-overlay-container').contains('mat-option', curatedUgcType).click();
 }
