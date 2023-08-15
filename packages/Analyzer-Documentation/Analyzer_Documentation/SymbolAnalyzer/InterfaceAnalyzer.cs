b9ccc78b9c2ea55186720d11364dcefc2350be87
David Sharer <sharerdavid@microsoft.com>
2023-08-15 16:50:54 -0700

    Analyze interfaces

--- packages/Analyzer-Documentation/Analyzer_Documentation/SymbolAnalyzer/InterfaceAnalyzer.cs
+++ packages/Analyzer-Documentation/Analyzer_Documentation/SymbolAnalyzer/InterfaceAnalyzer.cs
@@ -4,16 +4,16 @@ using Microsoft.CodeAnalysis.Diagnostics;
 namespace Analyzer_Documentation.SymbolAnalyzer
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class StructAnalyzer : BaseSymbolAnalyzer
+    public class InterfaceAnalyzer : BaseSymbolAnalyzer
     {
-        public override string DiagnosticId { get; }  = "DS1003";
+        public override string DiagnosticId { get; }  = "DS1002";
 
         // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
         // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        protected override string Title { get; } = "Struct lacks documentation";
-        protected override string MessageFormat { get; } = "Struct '{0}' lacks documentation";
-        protected override string Description { get; } = "Structes should have documentation";
+        protected override string Title { get; } = "Interface lacks documentation";
+        protected override string MessageFormat { get; } = "Interface '{0}' lacks documentation";
+        protected override string Description { get; } = "Interfaces should have documentation";
 
-        protected override TypeKind TargetTypeKind => TypeKind.Struct;
+        protected override TypeKind TargetTypeKind => TypeKind.Interface;
     }
 }
