b71c6e53fbba66afa2851d52b32a8bcbc2447309
David Sharer <sharerdavid@microsoft.com>
2023-08-17 11:53:05 -0700

    PR Comments

--- packages/Analyzer-Documentation/Analyzer_Documentation/MethodAnalyzer.cs
+++ packages/Analyzer-Documentation/Analyzer_Documentation/MethodAnalyzer.cs
@@ -9,8 +9,6 @@ namespace Analyzer_Documentation
     {
         public const string DiagnosticId = "DS2000";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private const string Title = "Visible method lacks documentation";
         private const string MessageFormat = "Method '{0}' in '{1}' lacks documentation ({2})";
         private const string Description = "Visible methods should have documentation";
@@ -33,14 +31,11 @@ namespace Analyzer_Documentation
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var methodSymbol = (IMethodSymbol)context.Symbol;
             var methodClass = methodSymbol.ContainingType;
             var methodClassName = methodClass?.Name;
@@ -51,7 +46,6 @@ namespace Analyzer_Documentation
 
             var isPropertyMethod = methodSymbol.MethodKind is MethodKind.PropertyGet or MethodKind.PropertySet;
             var isConstructorMethod = methodSymbol.MethodKind == MethodKind.Constructor;
-            //var isOrdinaryMethod = methodSymbol.MethodKind == MethodKind.Ordinary;
             var isRelevantMethod = !(isPropertyMethod || isConstructorMethod);
 
             var documentation = methodSymbol.GetDocumentationCommentXml();
--- packages/Analyzer-Documentation/Analyzer_Documentation/SymbolAnalyzer/BaseSymbolAnalyzer.cs
+++ packages/Analyzer-Documentation/Analyzer_Documentation/SymbolAnalyzer/BaseSymbolAnalyzer.cs
@@ -9,8 +9,6 @@ namespace Analyzer_Documentation.SymbolAnalyzer
     {
         public abstract string DiagnosticId { get; }
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
         protected abstract string Title { get; }
         protected abstract string MessageFormat { get; }
         protected abstract string Description { get; }
@@ -42,14 +40,11 @@ namespace Analyzer_Documentation.SymbolAnalyzer
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
 
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
             context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
         }
 
         private void AnalyzeSymbol(SymbolAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
             var isTarget = namedTypeSymbol.TypeKind == this.TargetTypeKind;
 
