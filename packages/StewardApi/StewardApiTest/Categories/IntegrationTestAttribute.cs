91c4ee219d83dc139c69190f7d505849f2f917da
David Sharer <sharerdavid@microsoft.com>
2023-08-17 14:09:08 -0700

    Correct documentation

--- packages/StewardApi/StewardApiTest/Categories/IntegrationTestAttribute.cs
+++ packages/StewardApi/StewardApiTest/Categories/IntegrationTestAttribute.cs
@@ -3,7 +3,12 @@
 namespace Turn10.LiveOps.StewardTest.Categories
 {
     /// <summary>
-    ///     Marks this class/method as a Unit Test.
+    ///     Marks this class/method as an Integration Test.
+    ///     Attribute should  be avoided in favor of more specific tags like
+    ///     <see cref="IntegrationTestDailyAttribute"/>,
+    ///     <see cref="IntegrationTestReleaseAttribute"/>,
+    ///     or <see cref="IntegrationTestDailyOrReleaseAttribute"/>.
+    ///     
     /// </summary>
     [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
     public sealed class IntegrationTestAttribute : TestAttributeBase
