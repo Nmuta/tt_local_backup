8935dcd00d1336448e000ad0dbdf899c93846fdb
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-08-17 16:08:38 -0700

    PR feedback

--- packages/StewardApi/StewardApi/Controllers/v2/Steelhead/Player/InventoryController.cs
+++ packages/StewardApi/StewardApi/Controllers/v2/Steelhead/Player/InventoryController.cs
@@ -213,7 +213,7 @@ namespace Turn10.LiveOps.StewardApi.Controllers.V2.Steelhead.Player
         [Authorize(Policy = UserAttributeValues.ManagePlayerInventory)]
         public async Task<IActionResult> EditPlayerProfileItems(ulong xuid, string externalProfileId, [FromBody] SteelheadPlayerInventory inventoryUpdates)
         {
-            var externalProfileGuid = externalProfileId.TryParseGuidElseThrow(nameof(externalProfileId));
+            var externalProfileIdGuid = externalProfileId.TryParseGuidElseThrow(nameof(externalProfileId));
 
             await this.Services.EnsurePlayerExistAsync(xuid).ConfigureAwait(true);
 
