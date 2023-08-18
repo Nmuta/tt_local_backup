6a62721d56e1a8c5b649a031c750320ec2e734d7
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-08-18 15:52:57 -0700

    check V1 and V2 endpoint headers before resorting to default endpoints

--- packages/StewardApi/StewardApi/Helpers/HttpRequestExtensions.cs
+++ packages/StewardApi/StewardApi/Helpers/HttpRequestExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using Castle.Core.Internal;
 using Microsoft.AspNetCore.Http;
 using Turn10.Data.Common;
 using Turn10.LiveOps.StewardApi.Contracts.Apollo;
@@ -20,20 +22,54 @@ namespace Turn10.LiveOps.StewardApi.Helpers
         /// </summary>
         public static string GetEndpoint(this HttpRequest request, TitleCodeName title)
         {
-            var defaultEndpoint = GetDefaultEndpoint(title);
+            var v1HeaderValue = GetV1EndpointHeaderValue(request, title);
+            var v2HeaderValue = GetV2EndpointHeaderValue(request, title);
+
+            string headerValue;
+            if (v1HeaderValue.IsNullOrEmpty() && v2HeaderValue.IsNullOrEmpty())
+            {
+                headerValue = GetDefaultEndpoint(title);
+            }
+            else
+            {
+                headerValue = !v2HeaderValue.IsNullOrEmpty() ? v2HeaderValue : v1HeaderValue;
+            }
+
+            return GetEndpointFromKey(headerValue, title);
+        }
+
+        /// <summary>
+        ///    Retrieves value for V1 endpoint header.
+        /// </summary>
+        private static string GetV1EndpointHeaderValue(HttpRequest request, TitleCodeName title)
+        {
+            var titleString = Enum.GetName(typeof(TitleCodeName), title);
 
             if (!request.Headers.TryGetValue("endpointKey", out var headerValue))
             {
-                return defaultEndpoint;
+                return string.Empty;
             }
 
-            var endpointKeyValue = headerValue.ToString();
-            endpointKeyValue.ShouldNotBeNullEmptyOrWhiteSpace(nameof(endpointKeyValue));
-
-            var splitValue = endpointKeyValue.Split('|');
+            // Strip out title from "Title|Endpoint"
+            var splitValue = headerValue.ToString().Split('|');
             var key = splitValue.ElementAtOrDefault(1);
 
-            return GetEndpointFromKey(key, title);
+            return key;
+        }
+
+        /// <summary>
+        ///    Retrieves value for V2 title specific endpoint header.
+        /// </summary>
+        private static string GetV2EndpointHeaderValue(HttpRequest request, TitleCodeName title)
+        {
+            var titleString = Enum.GetName(typeof(TitleCodeName), title);
+
+            if (!request.Headers.TryGetValue($"Endpoint-{titleString}", out var headerValue))
+            {
+                return string.Empty;
+            }
+
+            return headerValue;
         }
 
         private static string GetEndpointFromKey(string key, TitleCodeName title)
