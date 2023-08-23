03f89949ff16901aa625b186bc854e18e0a54f24
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-08-23 16:38:15 -0700

    PR feedback

--- packages/StewardApi/StewardApi/Contracts/BigCat/BigCatProductInfo.cs
+++ packages/StewardApi/StewardApi/Contracts/BigCat/BigCatProductInfo.cs
@@ -14,6 +14,7 @@ namespace Turn10.LiveOps.StewardApi.Contracts.BigCat
         /// <summary>
         ///     Builds cache key for Big Cat Product Info.
         /// </summary>
+        /// <param name="productId">This is the product ID used to find the name and prices.</param>
         public static string BuildCacheKey(string productId)
         {
             return $"BigCatProductInfo_{productId}";
