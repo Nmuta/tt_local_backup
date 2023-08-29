98ecd200d5810379cd9b8b07f66dd27ad48bc7af
Sebastian <v-scushman@microsoft.com>
2023-08-29 10:01:53 -0700

    Refactor Cypress Tests for Steward Homepage

--- packages/StewardClient-Cypress/cypress/e2e/integration/steward-core/nav.spec.ts
+++ packages/StewardClient-Cypress/cypress/e2e/integration/steward-core/nav.spec.ts
@@ -8,21 +8,21 @@ import { Tag, withTags } from '@support/tags';
 
 //These values may change as tools and games are added or removed from Steward
 const filterValues = {
-  allTools: '33',
+  allTools: '35',
   specificTool: '1',
   playerFilter: '7',
-  fh5Filter: '23',
+  fh5Filter: '24',
   playerFM7Filter: '5',
 };
 
-context('Steward Index', withTags(Tag.UnitTestStyle), () => {
-  beforeEach(() => {
+context('Steward Index', () => {
+  before(() => {
     login();
 
     cy.visit('/');
   });
 
-  it('should lead to Tools homepage', () => {
+  it('should lead to Tools homepage and close the tutorial', () => {
     // Verfiy cards
     cy.get('.mat-card-title').contains('Player Details');
     cy.get('.mat-card-title').contains('UGC Search');
@@ -33,6 +33,8 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
     cy.get('.mat-icon').contains('sticky_note_2');
     cy.get('.mat-icon').contains('account_circle');
     cy.get('.mat-icon').contains('contact_support');
+
+    cy.get('mat-card').contains('mat-icon', 'close').click();
   });
 
   context('External Tools', () => {
@@ -60,10 +62,11 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
   context('Search and Filters', () => {
     it('should search for a specific tool', () => {
       waitForProgressSpinners();
+      cy.contains('mat-icon', 'close').click();
       const $chipList = cy.get('mat-chip-list');
       $chipList.within(() => {
         const $inputChipList = cy.get('input');
-        $inputChipList.click().type('Player Details{enter}');
+        $inputChipList.type('Player Details{enter}');
       });
 
       waitForProgressSpinners();
@@ -74,10 +77,11 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
 
     it('should filter tools by input', () => {
       waitForProgressSpinners();
+      cy.contains('mat-icon', 'close').click();
       const $chipList = cy.get('mat-chip-list');
       $chipList.within(() => {
         const $inputChipList = cy.get('input');
-        $inputChipList.click().type('Player{enter}');
+        $inputChipList.type('Player{enter}');
       });
 
       waitForProgressSpinners();
@@ -85,67 +89,52 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
       cy.get('span').contains('Tools below do not match filters').should('exist');
     });
 
-    it('should filter tools by title', withTags(Tag.Broken), () => {
+    it('should filter tools by title', () => {
       waitForProgressSpinners();
+      cy.contains('mat-icon', 'close').click();
       const $chipList = cy.get('mat-chip-list');
       $chipList.within(() => {
         const $inputChipList = cy.get('input');
         $inputChipList.click();
       });
-      const $optionList = cy.get('mat-option');
-      $optionList.within(() => {
-        const $option = cy.get('span').contains('FH5');
-        $option.click();
-      });
+      cy.get('mat-option').contains('span', 'FH5').click();
 
       waitForProgressSpinners();
       cy.get('mat-chip').contains(filterValues.fh5Filter).should('exist');
       cy.get('span').contains('Tools below do not match filters').should('exist');
     });
 
-    it('should filter tools with multiple filters', withTags(Tag.Broken), () => {
+    it('should filter tools with multiple filters', () => {
       waitForProgressSpinners();
+      cy.contains('mat-icon', 'close').click();
       const $chipList = cy.get('mat-chip-list');
       $chipList.within(() => {
         const $inputChipList = cy.get('input');
-        $inputChipList.click().type('Player{enter}');
+        $inputChipList.type('Player{enter}');
         $inputChipList.click();
       });
-      const $optionList = cy.get('mat-option');
-      $optionList.within(() => {
-        const $option = cy.get('span').contains('FM7');
-        $option.click();
-      });
+      cy.get('mat-option').contains('span', 'FM7').click();
 
       waitForProgressSpinners();
       cy.get('mat-chip').contains(filterValues.playerFM7Filter).should('exist');
       cy.get('span').contains('Tools below do not match filters').should('exist');
-    });
-
-    it('should reset when filters are removed', withTags(Tag.Broken), () => {
-      waitForProgressSpinners();
-      const $chipList = cy.get('mat-chip-list');
-      $chipList.within(() => {
-        const $inputChipList = cy.get('input');
-        $inputChipList.click().type('Player{enter}');
-        $inputChipList.click();
-      });
-      const $optionList = cy.get('mat-option');
-      $optionList.within(() => {
-        const $option = cy.get('span').contains('FM7');
-        $option.click();
-      });
-
-      waitForProgressSpinners();
       cy.contains('mat-icon', 'close').click();
-      cy.get('mat-chip').contains(filterValues.allTools).should('exist');
-      cy.get('span').contains('Tools below do not match filters').should('not.exist');
     });
   });
 
   context('Nav Bar', () => {
+    it('should reset nav bar when pressing the reset selection button', () => {
+      cy.contains('mat-icon', 'close').click();
+      cy.get('button').contains('span', 'Reset Selection').click();
+
+      cy.get('mat-toolbar').within(() => {
+        cy.get('button').contains('span', 'Click to set standard tools').should('exist');
+      });
+    });
+
     it('should set Standard Tools', () => {
       waitForProgressSpinners();
+      cy.contains('mat-icon', 'close').click();
       cy.get('mat-toolbar').within(() => {
         cy.get('button').contains('span', 'Click to set standard tools').click();
 
@@ -159,124 +148,99 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
       });
     });
 
-    it('should swap between dark mode, light mode, and match system', withTags(Tag.Broken), () => {
-      waitForProgressSpinners();
-      cy.get('button').contains('span', 'Click to set standard tools').click();
-
+    it('should swap between dark mode, light mode, and match system', () => {
       cy.get('button')
         .contains('mat-icon', 'light_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'dark_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'devices')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="true"]')
         .should('exist');
 
       cy.get('button').contains('mat-icon', 'dark_mode').click();
       cy.get('button')
         .contains('mat-icon', 'light_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'dark_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="true"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'devices')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
 
       cy.get('button').contains('mat-icon', 'light_mode').click();
       cy.get('button')
         .contains('mat-icon', 'light_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="true"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'dark_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'devices')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
 
       cy.get('button').contains('mat-icon', 'devices').click();
       cy.get('button')
         .contains('mat-icon', 'light_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'dark_mode')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="false"]')
         .should('exist');
       cy.get('button')
         .contains('mat-icon', 'devices')
-        .parent()
-        .parent()
+        .parents('button')
         .get('[aria-pressed="true"]')
         .should('exist');
     });
 
-    it('should add tools to the nav bar', withTags(Tag.Broken), () => {
+    it('should add tools to the nav bar', () => {
       waitForProgressSpinners();
-      const $matCardList = cy.get('mat-card');
-      $matCardList.within(() => {
-        //get the mat-card for Player Details
-        const $playerDetailsCard = cy
-          .contains('mat-card-title', 'Player Details')
-          .parent()
-          .parent()
-          .parent();
-        $playerDetailsCard.within(() => {
-          cy.get('mat-card-actions').within(() => {
-            cy.get('mat-checkbox').click();
-          });
-        });
+      cy.get('button').contains('span', 'Reset Selection').click();
 
-        //get the mat-card for Gifting
-        const $giftingCard = cy.contains('mat-card-title', 'Gifting').parent().parent().parent();
-        $giftingCard.within(() => {
-          cy.get('mat-card-actions').within(() => {
-            cy.get('mat-checkbox').click();
-          });
+      const $playerDetailsCard = cy
+        .contains('mat-card-title', 'Player Details')
+        .parents('mat-card');
+      $playerDetailsCard.within(() => {
+        cy.get('mat-card-actions').within(() => {
+          cy.get('input').check({ force: true });
         });
+      });
 
-        //get the mat-card for Car Details
-        const $carDetailsCard = cy
-          .contains('mat-card-title', 'Car Details')
-          .parent()
-          .parent()
-          .parent();
-        $carDetailsCard.within(() => {
-          cy.get('mat-card-actions').within(() => {
-            cy.get('mat-checkbox').click();
-          });
+      const $giftingCard = cy.contains('mat-card-title', 'Gifting').parents('mat-card');
+      $giftingCard.within(() => {
+        cy.get('mat-card-actions').within(() => {
+          cy.get('input').check({ force: true });
+        });
+      });
+
+      const $carDetailsCard = cy.contains('mat-card-title', 'Car Details').parents('mat-card');
+      $carDetailsCard.within(() => {
+        cy.get('mat-card-actions').within(() => {
+          cy.get('input').check({ force: true });
         });
       });
 
@@ -285,32 +249,16 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
         cy.get('a').contains('span', 'Gifting').should('exist');
         cy.get('a').contains('span', 'Car Details').should('exist');
       });
+
+      cy.get('button').contains('span', 'Reset Selection').click();
     });
-
-    it(
-      'should reset nav bar when pressing the reset selection button',
-      withTags(Tag.Broken),
-      () => {
-        waitForProgressSpinners();
-        cy.get('mat-toolbar').within(() => {
-          cy.get('button').contains('span', 'Click to set standard tools').click();
-          cy.get('button').contains('span', 'Click to set standard tools').should('not.exist');
-        });
-
-        cy.get('button').contains('span', 'Reset Selection').click();
-
-        cy.get('mat-toolbar').within(() => {
-          cy.get('button').contains('span', 'Click to set standard tools').should('exist');
-        });
-      },
-    );
 
     // Known failure currently, bug made to address later (drag and drop issues)
     it('should reorder nav bar', withTags(Tag.Broken), () => {
       waitForProgressSpinners();
-      cy.get('mat-toolbar').within(() => {
-        cy.get('button').contains('span', 'Click to set standard tools').click();
+      cy.get('button').contains('span', 'Click to set standard tools').click();
 
+      cy.get('mat-toolbar').within(() => {
         cy.get('div')
           .contains('a')
           .first()
@@ -336,19 +284,15 @@ context('Steward Index', withTags(Tag.UnitTestStyle), () => {
   });
 
   context('Endpoints', () => {
-    it(
-      'should change Woodstock endpoint from Retail to Studio and confirm the change in Player Details by checking a Retail and Studio account',
-      withTags(Tag.Broken),
-      () => {
-        changeEndpoint('Woodstock', 'Retail', 'Studio');
-        cy.visit(stewardUrls.tools.playerDetails.woodstock);
-        cy.get('a').contains('span', 'FH5').contains('span', 'Studio').should('exist');
-        searchByGtag(RetailUsers['chad'].gtag);
-        cy.contains('h2', 'Request failed').should('exist');
-        cy.get('mat-chip').contains('mat-icon', 'cancel').click();
-        searchByXuid('2814649032001718');
-        cy.get('player-identity-results').contains('span', '2814649032001718').should('exist');
-      },
-    );
+    it('should change Woodstock endpoint from Retail to Studio and confirm the change in Player Details by checking a Retail and Studio account', () => {
+      changeEndpoint('Woodstock', 'Retail', 'Studio');
+      cy.visit(stewardUrls.tools.playerDetails.woodstock);
+      cy.get('a').contains('span', 'FH5').contains('span', 'Studio').should('exist');
+      searchByGtag(RetailUsers['chad'].gtag);
+      cy.contains('h2', 'Request failed').should('exist');
+      cy.get('mat-chip').contains('mat-icon', 'cancel').click();
+      searchByXuid('2814649032001718');
+      cy.get('player-identity-results').contains('span', '2814649032001718').should('exist');
+    });
   });
 });
