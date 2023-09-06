651b51dca58967c25acfb8d859eac14c8786eebd
David Sharer <sharerdavid@microsoft.com>
2023-09-05 17:35:51 -0700

    Move profile overrides into Profile tool

--- packages/StewardClient/src/app/sidebars/profile/profile.component.ts
+++ packages/StewardClient/src/app/sidebars/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
 import { Router } from '@angular/router';
 import { BaseComponent } from '@components/base-component/base.component';
 import { Clipboard } from '@helpers/clipboard';
@@ -7,11 +8,13 @@ import { GameTitle, UserRole } from '@models/enums';
 import { Select, Store } from '@ngxs/store';
 import { V2UsersService } from '@services/api-v2/users/users.service';
 import { WoodstockPlayerNotificationsService } from '@services/api-v2/woodstock/player/notifications/woodstock-player-notifications.service';
+import { WindowService } from '@services/window';
 import { EmailAddresses } from '@shared/constants';
 import { UserModel } from '@shared/models/user.model';
 import { ActionMonitor } from '@shared/modules/monitor-action/action-monitor';
-import { BreakAccessToken, LogoutUser, RecheckAuth } from '@shared/state/user/user.actions';
+import { ApplyProfileOverrides, BreakAccessToken, LogoutUser, RecheckAuth } from '@shared/state/user/user.actions';
 import { UserState } from '@shared/state/user/user.state';
+import { keys } from 'lodash';
 import { combineLatest, Observable } from 'rxjs';
 import { catchError, takeUntil } from 'rxjs/operators';
 
@@ -45,12 +48,18 @@ export class ProfileComponent extends BaseComponent implements OnInit {
 
   public getTeamLeadMonitor = new ActionMonitor(`GET user's team lead`);
 
+  public guestAccountStatus: undefined | boolean = undefined;
+  public activeRole: UserRole;
+  public showProfileOverrideOptions: boolean;
+  public roleList: UserRole[] = keys(UserRole).map(key => UserRole[key]);
+
   constructor(
     private readonly router: Router,
     private readonly store: Store,
     private readonly clipboard: Clipboard,
     private readonly woodstockPlayerNotificationsService: WoodstockPlayerNotificationsService,
     private readonly v2UsersService: V2UsersService,
+    private readonly windowService: WindowService,
   ) {
     super();
   }
@@ -81,6 +90,13 @@ export class ProfileComponent extends BaseComponent implements OnInit {
           this.loading = false;
         },
       );
+
+    const profile = this.store.selectSnapshot<UserModel>(UserState.profileForceTrueData); // Force true data so live ops admins can change their settings around
+    this.showProfileOverrideOptions = profile.role === UserRole.LiveOpsAdmin;
+    if (this.showProfileOverrideOptions) {
+      this.activeRole = profile.overrides?.role;
+      this.guestAccountStatus = profile.overrides?.isMicrosoftEmail;
+    }
   }
 
   /** Copys the state access token to clipboard */
@@ -146,6 +162,22 @@ export class ProfileComponent extends BaseComponent implements OnInit {
         }
 
         this.deleteNotificationsFormControls.xuid.setValue(null);
+      });
+  }
+
+  /** Sets the new active role to the live ops secondary role in state profile. */
+  public changeActiveRole($event: MatSelectChange): void {
+    this.store.dispatch(new ApplyProfileOverrides({ role: $event.value })).subscribe(() => {
+      this.windowService.location().reload();
+    });
+  }
+
+  /** Sets the new active role to the live ops secondary role in state profile. */
+  public changeGuestAccountStatus($event: MatSelectChange): void {
+    this.store
+      .dispatch(new ApplyProfileOverrides({ isMicrosoftEmail: $event.value }))
+      .subscribe(() => {
+        this.windowService.location().reload();
       });
   }
 
