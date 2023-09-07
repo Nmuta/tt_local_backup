c8ed73581ebcc9ed63c7bb70a60083e1aecd4363
David Sharer <sharerdavid@microsoft.com>
2023-09-07 15:31:31 -0700

    lint

--- packages/StewardClient/src/environments/app-data/changelog/2023-Q3-LATEST.changelog.ts
+++ packages/StewardClient/src/environments/app-data/changelog/2023-Q3-LATEST.changelog.ts
@@ -14,8 +14,8 @@ export const CHANGELOG_2023_Q3: ChangelogGroup = {
       longText: [
         'Toolbar now defaults to a set of standard tools for new users.',
         'Reset Tools button on the homepage now sets tools to the standard set of tools.',
-        'Clear Tools button added. It removes all tools in the toolbar.'
-      ]
+        'Clear Tools button added. It removes all tools in the toolbar.',
+      ],
     },
     {
       tag: ChangelogTag.General,
