3d011daa21009ccd0f4d28ab6a413c8a54de75d1
William Henning <v-whenning@microsoft.com>
2023-09-07 11:50:36 -0700

    Lint and remove tags from tests

--- packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/obligations/search.spec.ts
+++ packages/StewardClient-Cypress/cypress/e2e/integration/steward-tools/obligations/search.spec.ts
@@ -3,25 +3,24 @@ import { login } from '@support/steward/auth/login';
 import { stewardUrls } from '@support/steward/urls';
 import { disableFakeApi } from '@support/steward/util/disable-fake-api';
 import { clickTopLeftOfBody } from '@support/steward/util/click-top-left-of-body';
-import { withTags, Tag } from '@support/tags';
 import { waitForProgressSpinners } from '@support/steward/common/wait-for-progress-spinners';
 
 const expectedObligationValues = {
-  searchName : 'Franchise_Facts\n',
-  name : 'Telemetry_FactPlayerUsageDaily',
-  table : 'Telemetry_FactPlayerUsageDaily',
-  database : 'T10Analytics',
-  pipelineDependencies : '',
-  query : 'get_telemetry_fact_player_usage_daily',
-  startTime : '',
-  endTime : '',
-  maximumExecutionInterval : '1440',
-  executionInterval : '1440',
-  executionDelay : '4754',
-  parallelism : '4'
+  searchName: 'Franchise_Facts\n',
+  name: 'Telemetry_FactPlayerUsageDaily',
+  table: 'Telemetry_FactPlayerUsageDaily',
+  database: 'T10Analytics',
+  pipelineDependencies: '',
+  query: 'get_telemetry_fact_player_usage_daily',
+  startTime: '',
+  endTime: '',
+  maximumExecutionInterval: '1440',
+  executionInterval: '1440',
+  executionDelay: '4754',
+  parallelism: '4',
 };
 
-context('Steward / Tools / Obligation / Search', withTags(Tag.Broken, Tag.UnitTestStyle), () => {
+context('Steward / Tools / Obligation / Search', () => {
   before(() => {
     login();
 
@@ -49,7 +48,8 @@ context('Steward / Tools / Obligation / Search', withTags(Tag.Broken, Tag.UnitTe
     $firstActivity.should('exist');
 
     cy.contains('mat-expansion-panel', expectedObligationValues.name)
-    .first().contains('mat-form-field', 'Name');
+      .first()
+      .contains('mat-form-field', 'Name');
 
     $firstActivity.within(() => {
       cy.get('.mat-expansion-indicator').click();
