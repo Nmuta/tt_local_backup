e55ccc9cb9b3d19801fa9609aac6564a8213e048
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-09-07 14:46:46 -0700

    cleanup

--- packages/StewardApi/StewardApi/Contracts/Steelhead/SteelheadUserFlags.cs
+++ packages/StewardApi/StewardApi/Contracts/Steelhead/SteelheadUserFlags.cs
@@ -1,5 +1,7 @@
 ﻿#pragma warning disable SA1600 // Elements should be documented
 
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+
 namespace Turn10.LiveOps.StewardApi.Contracts.Steelhead
 {
     /// <summary>
@@ -7,24 +9,24 @@ namespace Turn10.LiveOps.StewardApi.Contracts.Steelhead
     /// </summary>
     public sealed class SteelheadUserFlags
     {
-        public SteelheadUserFlag IsGamecoreVip { get; set; }
+        public VerifiedUserFlag IsGamecoreVip { get; set; }
 
-        public SteelheadUserFlag IsGamecoreUltimateVip { get; set; }
+        public VerifiedUserFlag IsGamecoreUltimateVip { get; set; }
 
-        public SteelheadUserFlag IsSteamVip { get; set; }
+        public VerifiedUserFlag IsSteamVip { get; set; }
 
-        public SteelheadUserFlag IsSteamUltimateVip { get; set; }
+        public VerifiedUserFlag IsSteamUltimateVip { get; set; }
 
-        public SteelheadUserFlag IsTurn10Employee { get; set; }
+        public VerifiedUserFlag IsTurn10Employee { get; set; }
 
-        public SteelheadUserFlag IsEarlyAccess { get; set; }
+        public VerifiedUserFlag IsEarlyAccess { get; set; }
 
-        public SteelheadUserFlag IsUnderReview { get; set; }
+        public VerifiedUserFlag IsUnderReview { get; set; }
 
-        public SteelheadUserFlag IsRaceMarshall { get; set; }
+        public VerifiedUserFlag IsRaceMarshall { get; set; }
 
-        public SteelheadUserFlag IsContentCreator { get; set; }
+        public VerifiedUserFlag IsContentCreator { get; set; }
 
-        public SteelheadUserFlag IsCommunityManager { get; set; }
+        public VerifiedUserFlag IsCommunityManager { get; set; }
     }
 }
