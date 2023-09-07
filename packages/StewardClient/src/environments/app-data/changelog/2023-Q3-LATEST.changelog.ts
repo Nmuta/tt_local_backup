5bebbffafa4fbf5c6e757dccee2165a5a1f5fe65
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-09-07 16:00:04 -0700

    changelog entry

--- packages/StewardClient/src/environments/app-data/changelog/2023-Q3-LATEST.changelog.ts
+++ packages/StewardClient/src/environments/app-data/changelog/2023-Q3-LATEST.changelog.ts
@@ -8,6 +8,11 @@ export const CHANGELOG_2023_Q3: ChangelogGroup = {
   id: 'd519bdc3-1704-4d33-a637-3cb7a5929522',
   entries: [
     {
+      tag: { title: [GameTitle.FM8, GameTitle.FH5], tool: [NavbarTool.UgcDetails] },
+      uuid: '26e5ce44-0472-4b85-b1f9-cc2629b0e8b9',
+      shortText: 'Fixed bug preventing hide and unhide of UGC',
+    },
+    {
       tag: { title: [GameTitle.FM8], tool: NavbarTool.UnifiedCalendar },
       uuid: '34637ef2-c305-4514-bc20-a849bb90ede4',
       shortText: 'Add player and pegasus lookup inputs to all calendars',
