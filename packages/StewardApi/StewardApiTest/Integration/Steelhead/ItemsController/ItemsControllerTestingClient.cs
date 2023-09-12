1758d122cf6276584fa2c7881e76e06b21b7a9f0
Terrence Donnelly <v-tdonnelly@microsoft.com>
2023-09-12 12:07:18 -0500

    Implement ItemsController tests for Steelhead

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/ItemsController/ItemsControllerTestingClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class ItemsControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public ItemsControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<SteelheadMasterInventory> GetMasterInventoryList()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/items");
+
+            return await ServiceClient.SendRequestAsync<SteelheadMasterInventory>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<SimpleCar>> GetCarsAsync()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/items/cars");
+
+            return await ServiceClient.SendRequestAsync<IEnumerable<SimpleCar>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/ItemsController/ItemsControllerTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class ItemsControllerTests : SteelheadControllerTestsBase
+    {
+        private static ItemsControllerTestingClient stewardClient;
+        private static ItemsControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new ItemsControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new ItemsControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetMasterInventoryList()
+        {
+            try
+            {
+                var response = await stewardClient.GetMasterInventoryList().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetMasterInventoryList_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetMasterInventoryList().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetCarsAsync()
+        {
+            try
+            {
+                var response = await stewardClient.GetCarsAsync().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetCarsAsync_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetCarsAsync().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
