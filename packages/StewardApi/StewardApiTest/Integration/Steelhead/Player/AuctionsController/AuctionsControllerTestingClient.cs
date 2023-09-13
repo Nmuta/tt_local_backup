8940c4fcd0357bf893dc2901742f725c17e8ad36
Robert Schroeder <v-robertschr@microsoft.com>
2023-09-13 11:09:56 -0400

    create tests

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/AuctionsController/AuctionsControllerTestingClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+using Turn10.LiveOps.StewardApi.Contracts.Data;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class AuctionsControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public AuctionsControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<IList<PlayerAuction>> GetPlayerAuctions(
+            ulong xuid,
+            [FromQuery] short carId = short.MaxValue,
+            [FromQuery] short makeId = short.MaxValue,
+            [FromQuery] string status = "Any",
+            [FromQuery] string sort = "ClosingDateDescending")
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/auctions");
+
+            return await ServiceClient.SendRequestAsync<IList<PlayerAuction>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<IList<AuctionHistoryEntry>> GetPlayerAuctionLog(ulong xuid, [FromQuery] string skipToken = null)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/auctions");
+
+            return await ServiceClient.SendRequestAsync<IList<AuctionHistoryEntry>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/AuctionsController/AuctionsControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class AuctionsControllerTests : SteelheadControllerTestsBase
+    {
+        private static AuctionsControllerTestingClient stewardClient;
+        private static AuctionsControllerTestingClient unauthedClient;
+        private ulong xuid = 2675352635783107;
+        private string gamertag = "Plink";
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new AuctionsControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new AuctionsControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetPlayerAuctions()
+        {
+            try
+            {
+                var response = await stewardClient.GetPlayerAuctions(this.xuid).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetPlayerAuctionLog()
+        {
+            try
+            {
+                var response = await stewardClient.GetPlayerAuctionLog(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+    }
+}
