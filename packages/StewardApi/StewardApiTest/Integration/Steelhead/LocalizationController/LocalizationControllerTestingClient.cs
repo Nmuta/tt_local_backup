7dec8155a46e8faf45faa8447c54f3291891bc06
Sebastian <v-scushman@microsoft.com>
2023-09-14 08:49:27 -0700

    Add Integration Tests for LocalizationController

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/LocalizationController/LocalizationControllerTestingClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+using LiveOpsContracts = Turn10.LiveOps.StewardApi.Contracts.Common;
+using SteelheadLiveOpsContent;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class LocalizationControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public LocalizationControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<Dictionary<Guid, List<LiveOpsContracts.LocalizedString>>> GetLocalizedString()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/localization");
+
+            return await ServiceClient.SendRequestAsync<Dictionary<Guid, List<LiveOpsContracts.LocalizedString>>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<SupportedLocale>> GetSupportedLocales()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/localization/supportedLocales");
+
+            return await ServiceClient.SendRequestAsync<IEnumerable<SupportedLocale>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/LocalizationController/LocalizationControllerTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class LocalizationControllerTests : SteelheadControllerTestsBase
+    {
+        private static LocalizationControllerTestingClient stewardClient;
+        private static LocalizationControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new LocalizationControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new LocalizationControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetLocalizedStringData()
+        {
+            try
+            {
+                var response = await stewardClient.GetLocalizedString().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetLocalizedStringData_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetLocalizedString().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetSupportedLocales()
+        {
+            try
+            {
+                var response = await stewardClient.GetSupportedLocales().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetSupportedLocales_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetSupportedLocales().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
