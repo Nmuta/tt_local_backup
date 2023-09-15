2ae142db25c30ffe03d3081ed6285d24df9915a8
Robert Schroeder <v-robertschr@microsoft.com>
2023-09-15 10:23:29 -0400

    init tests

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/BanSummariesController/BanSummariesControllerTestingClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+using Turn10.Services.LiveOps.FH5_main.Generated;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class BanSummariesControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public BanSummariesControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<IList<BanSummary>> GetBanSummaries([FromBody] IList<ulong> xuids)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/banSummaries");
+
+            return await ServiceClient.SendRequestAsync<IList<BanSummary>>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/BanSummariesController/BanSummariesControllerTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+using Turn10.Services.LiveOps.FH5_main.Generated;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class BanSummariesControllerTests : SteelheadControllerTestsBase
+    {
+        private static BanSummariesControllerTestingClient stewardClient;
+        private static BanSummariesControllerTestingClient unauthedClient;
+        private static List<ulong> xuids;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new BanSummariesControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new BanSummariesControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+            // private static xuids = new List<ulong> {TestConstants.TestAccountXuid};
+            // private static invalidXuids = 
+
+           // private static xuids = new List<ulong> {2675352635783107};
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetBanSummaries()
+        {
+            try
+            {
+                var response = await stewardClient.GetBanSummaries(TestConstants., this.banconfig).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetBanSummaries_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.GetBanSummaries(new listTestConstants.InvalidXuid, this.banconfig).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetBanSummaries_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetBanSummaries(TestConstants., this.banconfig).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
