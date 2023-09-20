249b6493ce83c86cc02aedfc1e72492333fa7dda
David Sharer <sharerdavid@microsoft.com>
2023-09-20 09:04:25 -0700

    Fix tests

--- packages/StewardClient/src/app/shared/components/tour-step/tour-step.component.spec.ts
+++ packages/StewardClient/src/app/shared/components/tour-step/tour-step.component.spec.ts
@@ -1,18 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TourStepComponent } from './tour-step.component';
+import { createStandardTestModuleMetadataMinimal } from '@mocks/standard-test-module-metadata-minimal';
 
 describe('TourStepComponent', () => {
   let component: TourStepComponent;
   let fixture: ComponentFixture<TourStepComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TourStepComponent],
-    }).compileComponents();
+    await TestBed.configureTestingModule(
+      createStandardTestModuleMetadataMinimal({
+        imports: [TourStepComponent],
+      }),
+    ).compileComponents();
 
     fixture = TestBed.createComponent(TourStepComponent);
     component = fixture.componentInstance;
+    component.step = {};
     fixture.detectChanges();
   });
 
