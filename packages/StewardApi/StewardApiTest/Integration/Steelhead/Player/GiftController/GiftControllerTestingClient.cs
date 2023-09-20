f049f5c347b2c389ab4b29be15d9d0396073fa12
William Henning <v-whenning@microsoft.com>
2023-09-20 02:49:11 -0700

    Add gift controller integration tests

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/GiftController/GiftControllerTestingClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Antlr4.Runtime.Atn;
+using Microsoft.AspNetCore.Mvc;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+using Turn10.LiveOps.StewardApi.Contracts.Data;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class GiftControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public GiftControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        // Player gift tests
+
+        public async Task<BackgroundJobInternal> UpdateGroupInventoriesUseBackgroundProcessing(SteelheadGroupGift groupGift)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/players/gift/useBackgroundProcessing");
+
+         return await ServiceClient.SendRequestAsync<BackgroundJobInternal>(HttpMethod.Post, path, this.authKey, Version, groupGift, headers: this.headers).ConfigureAwait(false);
+    }
+
+        public async Task<BackgroundJobInternal> GiftLiveryToPlayersUseBackgroundProcessing(BulkLiveryGift<LocalizedMessageExpirableGroupGift> gift)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/players/gift/useBackgroundProcessing");
+
+            return await ServiceClient.SendRequestAsync<BackgroundJobInternal>(HttpMethod.Post, path, this.authKey, Version, gift, headers: this.headers).ConfigureAwait(false);
+        }
+
+        // Group gift tests
+
+        public async Task<GiftResponse<int>> UpdateGroupInventory(int groupId, SteelheadGift gift)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/group/{groupId}/gift");
+
+            return await ServiceClient.SendRequestAsync<GiftResponse<int>>(HttpMethod.Post, path, this.authKey, Version, gift, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<GiftResponse<int>> GiftLiveryToUserGroup(int groupId, BulkLiveryGift<LocalizedMessageExpirableGift> gift)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/group/{groupId}/gift/livery");
+
+            return await ServiceClient.SendRequestAsync<GiftResponse<int>>(HttpMethod.Post, path, this.authKey, Version, gift, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/GiftController/GiftControllerTests.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardApi.Contracts.Woodstock;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class GiftControllerTests : SteelheadControllerTestsBase
+    {
+        private static GiftControllerTestingClient stewardClient;
+        private static GiftControllerTestingClient unauthedClient;
+        //private static SteelheadGroupGift validGift;
+        //private static SteelheadGroupGift invalidGift;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new GiftControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new GiftControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+
+        }
+
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendPlayerGift_InvalidAuth()
+        {
+            var gift = this.CreateGroupGift(TestConstants.TestAccountXuid);
+
+            try
+            {
+                var response = await unauthedClient.UpdateGroupInventoriesUseBackgroundProcessing(gift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendPlayerGift_InvalidXuid()
+        {
+            var gift = this.CreateGroupGift(TestConstants.InvalidXuid);
+
+            try
+            {
+                var response = await stewardClient.UpdateGroupInventoriesUseBackgroundProcessing(gift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendPlayerLiveryGift_InvalidAuth()
+        {
+            var bulkgift = this.CreateBulkLiveryGroupGift(TestConstants.TestAccountXuid);
+
+            try
+            {
+                var response = await unauthedClient.GiftLiveryToPlayersUseBackgroundProcessing(bulkgift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendPlayerLiveryGift_InvalidXuid()
+        {
+            var bulkgift = this.CreateBulkLiveryGroupGift(TestConstants.InvalidXuid);
+
+            try
+            {
+                var response = await stewardClient.GiftLiveryToPlayersUseBackgroundProcessing(bulkgift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendGrouoGift_InvalidAuth()
+        {
+            var gift = this.CreateGift(TestConstants.TestAccountXuid);
+
+            try
+            {
+                var response = await unauthedClient.UpdateGroupInventory(31, gift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendGroupGift_InvalidGroupId()
+        {
+            var gift = this.CreateGift(TestConstants.InvalidXuid);
+
+            try
+            {
+                var response = await stewardClient.UpdateGroupInventory(-1, gift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendGroupLiveryGift_InvalidAuth()
+        {
+            var bulkgift = this.CreateBulkLiveryGift(TestConstants.TestAccountXuid);
+
+            try
+            {
+                var response = await unauthedClient.GiftLiveryToUserGroup(31,bulkgift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task SendGroupLiveryGift_InvalidGroupId()
+        {
+            var bulkgift = this.CreateBulkLiveryGift(TestConstants.InvalidXuid);
+            //bulkgift.LiveryIds = new List<string>();
+            //bulkgift.LiveryIds.Append("a9ffcef7-dac6-4364-8e0a-08017ae204f9");
+
+            try
+            {
+                var response = await stewardClient.GiftLiveryToUserGroup(-1, bulkgift).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, ex.StatusCode);
+            }
+        }
+
+        private SteelheadGroupGift CreateGroupGift(ulong xuid)
+        {
+            return new SteelheadGroupGift
+            {
+                Xuids = new List<ulong>
+                {
+                    xuid,
+                },
+                GiftReason = "Integration Test",
+                Inventory = this.CreateGiftInventory(),
+            };
+        }
+
+        private SteelheadMasterInventory CreateGiftInventory()
+        {
+            var giftInventory = new SteelheadMasterInventory
+            {
+                CreditRewards =
+                    new List<MasterInventoryItem>
+                    {
+                        new MasterInventoryItem {Id = -1, Description = "Credits", Quantity = 1},
+                    },
+                Cars = new List<MasterInventoryItem> { new MasterInventoryItem { Id = 2870, Quantity = 1 } },
+            };
+
+            return giftInventory;
+        }
+
+        private BulkLiveryGift<LocalizedMessageExpirableGroupGift> CreateBulkLiveryGroupGift (ulong xuid)
+        {
+            return new BulkLiveryGift<LocalizedMessageExpirableGroupGift>
+            {
+                Target = this.CreateLiveryGift(xuid),
+            };
+        }
+
+        private LocalizedMessageExpirableGroupGift CreateLiveryGift(ulong xuid)
+        {
+            return new LocalizedMessageExpirableGroupGift
+            {
+                Xuids = new List<ulong>
+                {
+                    xuid,
+                },
+                GiftReason = "Integration Test",
+            };
+        }
+
+        private SteelheadGift CreateGift(ulong xuid)
+        {
+            return new SteelheadGift
+            {
+                GiftReason = "Integration Test",
+                Inventory = this.CreateGiftInventory(),
+            };
+
+        }
+
+        private BulkLiveryGift<LocalizedMessageExpirableGift> CreateBulkLiveryGift(ulong xuid)
+        {
+            return new BulkLiveryGift<LocalizedMessageExpirableGift>
+            {
+                Target = this.CreateLiveryGift(xuid),
+            };
+        }
+
+    }
+}
