2b0acf6157b3249b753fcde5b81ca4b8e48a2288
David Sharer <sharerdavid@microsoft.com>
2023-09-20 14:48:11 -0700

    Fix tests

--- packages/StewardClient/src/app/sidebars/changelog/changelog-entry-old/changelog-entry-old.component.spec.ts
+++ packages/StewardClient/src/app/sidebars/changelog/changelog-entry-old/changelog-entry-old.component.spec.ts
@@ -1,18 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChangelogEntryOldComponent } from './changelog-entry-old.component';
+import { createStandardTestModuleMetadata } from '@mocks/standard-test-module-metadata';
+import { ChangelogTag } from '@environments/app-data/changelog';
 
 describe('ChangelogEntryOldComponent', () => {
   let component: ChangelogEntryOldComponent;
   let fixture: ComponentFixture<ChangelogEntryOldComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ChangelogEntryOldComponent],
-    }).compileComponents();
+    await TestBed.configureTestingModule(
+      createStandardTestModuleMetadata({
+        declarations: [ChangelogEntryOldComponent],
+      }),
+    ).compileComponents();
 
     fixture = TestBed.createComponent(ChangelogEntryOldComponent);
     component = fixture.componentInstance;
+    component.entry = {
+      tag: ChangelogTag.All,
+      shortText: 'test',
+      uuid: '53c2eebd-1329-4f11-ad0b-cec5f69658c0',
+    };
     fixture.detectChanges();
   });
 
