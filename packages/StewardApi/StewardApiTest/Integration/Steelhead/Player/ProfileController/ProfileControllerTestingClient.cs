cff0d9663b7fa2d2e7b9e476dd2e4668882ede02
Sebastian <v-scushman@microsoft.com>
2023-09-22 13:58:26 -0700

    Add Integration Tests for ProfileController

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/ProfileController/ProfileControllerTestingClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class ProfileControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public ProfileControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<OkResult> PostSavePlayerProfile(ulong xuid, ulong profileId)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/profile/{profileId}/save");
+
+            return await ServiceClient.SendRequestAsync<OkResult>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<OkResult> PostLoadPlayerProfile(ulong xuid, ulong profileId)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/profile/{profileId}/load");
+
+            return await ServiceClient.SendRequestAsync<OkResult>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<OkResult> PostResetPlayerProfile(ulong xuid, ulong profileId)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/profile/{profileId}/reset");
+
+            return await ServiceClient.SendRequestAsync<OkResult>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/ProfileController/ProfileControllerTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class ProfileControllerTests : SteelheadControllerTestsBase
+    {
+        private static ProfileControllerTestingClient stewardClient;
+        private static ProfileControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new ProfileControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new ProfileControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostSavePlayerProfile_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostSavePlayerProfile(TestConstants.TestAccountXuid, TestConstants.TestAccountProfileId).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostLoadPlayerProfile_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostLoadPlayerProfile(TestConstants.TestAccountXuid, TestConstants.TestAccountProfileId).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostResetPlayerProfile_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostResetPlayerProfile(TestConstants.TestAccountXuid, TestConstants.TestAccountProfileId).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
