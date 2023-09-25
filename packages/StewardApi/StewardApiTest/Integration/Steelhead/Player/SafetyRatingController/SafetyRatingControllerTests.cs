26232137375da1b23094d341e108727d8d8849fc
Sebastian <v-scushman@microsoft.com>
2023-09-25 14:18:59 -0700

    Add Integration Tests for SafetyRatingController

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/SafetyRatingController/SafetyRatingControllerTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class SafetyRatingControllerTests : SteelheadControllerTestsBase
+    {
+        private static SafetyRatingControllerTestingClient stewardClient;
+        private static SafetyRatingControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new SafetyRatingControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new SafetyRatingControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetSafetyRating()
+        {
+            try
+            {
+                var response = await stewardClient.GetSafetyRating(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetSafetyRating_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.GetSafetyRating(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task DeleteSafetyRating_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.DeleteSafetyRating(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostSetSafetyRating_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.PostSetSafetyRating(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetSafetyRating_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetSafetyRating(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task DeleteSafetyRating_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.DeleteSafetyRating(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostSetSafetyRating_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostSetSafetyRating(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
