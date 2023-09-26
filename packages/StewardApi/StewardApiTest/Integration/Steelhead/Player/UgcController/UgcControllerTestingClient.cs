474c46c32a530c270525293087f18c1d1742267a
Sebastian <v-scushman@microsoft.com>
2023-09-26 15:42:32 -0700

    Add Tests File; Adjust TestingClient

--- packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/UgcController/UgcControllerTestingClient.cs
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Player/UgcController/UgcControllerTestingClient.cs
@@ -16,16 +16,16 @@ namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
             this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
         }
 
-        public async Task<IList<SteelheadUgcItem>> GetUgcItems(ulong xuid)
+        public async Task<IList<SteelheadUgcItem>> GetUgcItems(ulong xuid, string ugcType)
         {
-            var path = new Uri(this.baseUri, $"{TitlePath}player/{xuid}/ugc");
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/ugc?ugcType={ugcType}");
 
             return await ServiceClient.SendRequestAsync<IList<SteelheadUgcItem>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
         }
 
-        public async Task<IList<UgcItem>> GetHiddenUgcItems(ulong xuid)
+        public async Task<IList<UgcItem>> GetHiddenUgcItems(ulong xuid, string ugcType)
         {
-            var path = new Uri(this.baseUri, $"{TitlePath}player/{xuid}/ugc/hidden");
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/ugc/hidden?ugcType={ugcType}");
 
             return await ServiceClient.SendRequestAsync<IList<UgcItem>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
         }
