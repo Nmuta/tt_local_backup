76aab5f99656163bd5a462e29011cc9b3141a73f
Jordan Yates (KFORCE INC) <v-joyates@microsoft.com>
2023-09-27 14:59:45 -0700

    retry sending failed gifts.

---
+++ packages/StewardApi/StewardApi/Contracts/Common/FailedGiftRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace Turn10.LiveOps.StewardApi.Contracts.Common
+{
+    /// <summary>
+    ///     Represents a failed request to send an item to a user.
+    /// </summary>
+    /// <typeparam name="T">The title specific InventoryItemType</typeparam>
+    public sealed class FailedGiftRequest<T>
+    {
+        /// <summary>
+        ///     Gets or sets the type.
+        /// </summary>
+        public T Type { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the item.
+        /// </summary>
+        public MasterInventoryItem Item { get; set; }
+    }
+}
--- packages/StewardApi/StewardApi/Providers/Woodstock/WoodstockPlayerInventoryProvider.cs
+++ packages/StewardApi/StewardApi/Providers/Woodstock/WoodstockPlayerInventoryProvider.cs
@@ -411,24 +411,29 @@ namespace Turn10.LiveOps.StewardApi.Providers.Woodstock
             IDictionary<InventoryItemType, IList<MasterInventoryItem>> inventoryGifts,
             IDictionary<InventoryItemType, MasterInventoryItem> currencyGifts)
         {
+            var failedRequests = new List<FailedGiftRequest<InventoryItemType>>();
             var errors = new List<StewardError>();
             foreach (var (key, value) in inventoryGifts)
             {
                 foreach (var item in value)
                 {
-                    try
+                    var failedQuantity = 0;
+                    for (var i = 0; i < item.Quantity; i++)
                     {
-                        for (var i = 0; i < item.Quantity; i++)
+                        try
                         {
                             await serviceCall(key, item.Id).ConfigureAwait(false);
                         }
+                        catch
+                        {
+                             failedQuantity++;
+                        }
                     }
-                    catch (Exception ex)
+
+                    if (failedQuantity > 0)
                     {
-                        this.loggingService.LogException(new AppInsightsException($"Failed to gift item to player. (type: {key}) (itemId: {item.Id})", ex));
-                        var error = new FailedToSendStewardError($"Failed to send item of type: {key} with ID: {item.Id}");
-                        item.Error = error;
-                        errors.Add(error);
+                        this.loggingService.LogException(new AppInsightsException($"Failed to gift item to player. (type: {key}) (itemId: {item.Id})"));
+                        failedRequests.Add(new FailedGiftRequest<InventoryItemType> { Type = key, Item = new MasterInventoryItem { Id = item.Id, Quantity = failedQuantity } });
                     }
                 }
             }
@@ -460,9 +465,32 @@ namespace Turn10.LiveOps.StewardApi.Providers.Woodstock
 
                 if (failedToSendAmount > 0)
                 {
-                    var error = new FailedToSendStewardError($"Failed to send {failedToSendAmount} of type: {key}");
+                    var error = new FailedToSendStewardError($"Failed to send currency (Quantity: {failedToSendAmount}) (Type: {key})");
                     value.Error = error;
                     errors.Add(error);
+                }
+            }
+
+            // Code below waits a few seconds and then retries sending failed gifts. This is in place to resolve an intermittent failure that LiveOps/Services have yet to diagnose and fix.
+            if (failedRequests.Count > 0)
+            {
+               await Task.Delay(3_000);
+            }
+
+            foreach (var request in failedRequests)
+            {
+                for (var i = 0; i < request.Item.Quantity; i++)
+                {
+                    try
+                    {
+                        await serviceCall(request.Type, request.Item.Id).ConfigureAwait(false);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.loggingService.LogException(new AppInsightsException($"Failed to gift item to player on retry. (type: {request.Type}) (itemId: {request.Item.Id})", ex));
+                        var error = new FailedToSendStewardError($"Failed to send item. (Type: {request.Type}) (ID: {request.Item.Id})", ex);
+                        errors.Add(error);
+                    }
                 }
             }
 
