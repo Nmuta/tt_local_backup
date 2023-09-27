f052a3d8d4efc34a1d20809fb0822ede486f9b53
Sebastian <v-scushman@microsoft.com>
2023-09-27 09:50:38 -0700

    Add Integration Tests for IdentitiesController

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Players/IdentitiesController/IdentitiesControllerTestingClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead.RacersCup;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class IdentitiesControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public IdentitiesControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<IList<IdentityResultAlpha>> GetPlayersIdentities()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/players/identities");
+
+            return await ServiceClient.SendRequestAsync<IList<IdentityResultAlpha>>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Players/IdentitiesController/IdentitiesControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class IdentitiesControllerTests : SteelheadControllerTestsBase
+    {
+        private static IdentitiesControllerTestingClient stewardClient;
+        private static IdentitiesControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new IdentitiesControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new IdentitiesControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetPlayersIdentities_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetPlayersIdentities().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
