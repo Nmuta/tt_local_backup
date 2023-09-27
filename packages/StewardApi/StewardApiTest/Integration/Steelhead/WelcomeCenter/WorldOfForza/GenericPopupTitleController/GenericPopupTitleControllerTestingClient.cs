a3b1a77c125e722a4e2678d07f808fcbeab30d1a
Robert Schroeder <v-robertschr@microsoft.com>
2023-09-27 11:25:20 -0400

    add genericTile tests

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/WelcomeCenter/WorldOfForza/GenericPopupTitleController/GenericPopupTitleControllerTestingClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Git;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead.WelcomeCenter.WorldOfForza;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class GenericPopupTitleControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public GenericPopupTitleControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<Dictionary<Guid, string>> GetWorldOfForzaDeeplinkSelectionOptionsAsync()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/genericpopup/options");
+
+            return await ServiceClient.SendRequestAsync<Dictionary<Guid, string>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<WofGenericPopupBridge> GetWorldOfForzaDeeplinkCurrentValuesAsync(string id)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/genericpopup/{id}");
+
+            return await ServiceClient.SendRequestAsync<WofGenericPopupBridge>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<PullRequest> EditAndSubmitGenericPopupTitle(string id, [FromBody] WofGenericPopupBridge wofTileBridge)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/genericpopup/{id}");
+
+            return await ServiceClient.SendRequestAsync<PullRequest>(HttpMethod.Post, path, this.authKey, Version, wofTileBridge, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/WelcomeCenter/WorldOfForza/GenericPopupTitleController/GenericPopupTitleControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead.WelcomeCenter.WorldOfForza;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class GenericPopupTitleTests : SteelheadControllerTestsBase
+    {
+        private static GenericPopupTitleTestingClient stewardClient;
+        private static GenericPopupTitleTestingClient unauthedClient;
+        private static WofGenericPopupBridge bridge;
+        private static id;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new GenericPopupTitleTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new GenericPopupTitleTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+            bridge = new WofGenericPopupBridge();
+            id = "someIdtobedetermined"
+            
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaGenericPopupSelectionOptionsAsync()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaGenericPopupSelectionOptionsAsync().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaGenericPopupSelectionOptionsAsync_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetWorldOfForzaGenericPopupSelectionOptionsAsync().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaGenericCurrentValuesAsync()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaGenericCurrentValuesAsync(id).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaGenericCurrentValuesAsync_InvalidId()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaGenericCurrentValuesAsync("invalidID").ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaGenericCurrentValuesAsync_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetWorldOfForzaGenericCurrentValuesAsync(id).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task EditAndSubmitGenericPopupTile_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.EditAndSubmitGenericPopupTile(id, bridge).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
