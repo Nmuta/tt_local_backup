2e33abe745d5c723e8c780f41970c593c4e8ceba
Robert Schroeder <v-robertschr@microsoft.com>
2023-09-27 11:10:11 -0400

    add tests

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/WelcomeCenter/WorldOfForza/DeeplinkTileControllerTestingClient.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Git;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead.WelcomeCenter.WorldOfForza;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    public sealed class DeeplinkTileControllerTestingClient : SteelheadStewardBaseTestingClient
+    {
+        public DeeplinkTileControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(SteelheadEndpoint.Retail));
+        }
+
+        public async Task<Dictionary<Guid, string>> GetWorldOfForzaDeeplinkSelectionOptionsAsync()
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/deeplink/options");
+
+            return await ServiceClient.SendRequestAsync<Dictionary<Guid, string>>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<WofDeeplinkBridge> GetWorldOfForzaDeeplinkCurrentValuesAsync(string id)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/deeplink/{id}");
+
+            return await ServiceClient.SendRequestAsync<WofDeeplinkBridge>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<PullRequest> EditAndSubmitDeeplinkTile(string id, [FromBody] WofDeeplinkBridge wofTileBridge)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}welcomecenter/worldofforza/deeplink/{id}");
+
+            return await ServiceClient.SendRequestAsync<PullRequest>(HttpMethod.Post, path, this.authKey, Version, wofTileBridge, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/WelcomeCenter/WorldOfForza/DeeplinkTileControllerTests.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Turn10.LiveOps.StewardApi.Contracts.Steelhead.WelcomeCenter.WorldOfForza;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class DeeplinkTileControllerTests : SteelheadControllerTestsBase
+    {
+        private static DeeplinkTileControllerTestingClient stewardClient;
+        private static DeeplinkTileControllerTestingClient unauthedClient;
+        private static WofDeeplinkBridge bridge;
+        private static id;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new DeeplinkTileControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new DeeplinkTileControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+            bridge = new WofDeeplinkBridge();
+            id = "someIdtobedetermined"
+            
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaDeeplinkSelectionOptionsAsync()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaDeeplinkSelectionOptionsAsync().ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaDeeplinkSelectionOptionsAsync_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetWorldOfForzaDeeplinkSelectionOptionsAsync().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaDeeplinkCurrentValuesAsync()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaDeeplinkCurrentValuesAsync(id).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaDeeplinkCurrentValuesAsync_InvalidId()
+        {
+            try
+            {
+                var response = await stewardClient.GetWorldOfForzaDeeplinkCurrentValuesAsync("invalidID").ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.BadRequest, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetWorldOfForzaDeeplinkCurrentValuesAsync_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetWorldOfForzaDeeplinkCurrentValuesAsync(id).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task EditAndSubmitDeeplinkTile_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.EditAndSubmitDeeplinkTile(id, bridge).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
