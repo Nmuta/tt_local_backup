e6d84d0eb3d44c27bdfec13102847bdd5b075e93
Sebastian <v-scushman@microsoft.com>
2023-09-28 15:37:36 -0700

    Add Integration Tests for SharecodeController

---
+++ packages/StewardApi/StewardApiTest/Integration/Steelhead/Ugc/SharecodeController/SharecodeControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Steelhead
+{
+    [TestClass]
+    public sealed class SharecodeControllerTests : SteelheadControllerTestsBase
+    {
+        private static SharecodeControllerTestingClient stewardClient;
+        private static SharecodeControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new SharecodeControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new SharecodeControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostGenerateSharecodes_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostGenerateSharecodes().ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
