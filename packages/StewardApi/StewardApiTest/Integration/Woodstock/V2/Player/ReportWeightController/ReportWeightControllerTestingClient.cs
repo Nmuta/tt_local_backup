f382fd92c9f3076ecfd22dfdc64b16282e8e1153
William Henning <v-whenning@microsoft.com>
2023-10-18 23:23:36 -0700

    Add Woodstock V2 integration tests for Report Weight Controller

---
+++ packages/StewardApi/StewardApiTest/Integration/Woodstock/V2/Player/ReportWeightController/ReportWeightControllerTestingClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardApi.Contracts.Woodstock;
+using Turn10.LiveOps.StewardApi.Contracts.Common;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Woodstock.V2
+{
+    public sealed class ReportWeightControllerTestingClient : WoodstockStewardBaseTestingClient
+    {
+        public ReportWeightControllerTestingClient(Uri baseUri, string authKey) : base(baseUri, authKey)
+        {
+            this.headers.Add("Endpoint-Steelhead", nameof(WoodstockEndpoint.Retail));
+        }
+
+        public async Task<UserReportWeight> GetReportWeight(ulong xuid)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/reportWeight");
+
+            return await ServiceClient.SendRequestAsync<UserReportWeight>(HttpMethod.Get, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+
+        public async Task<UserReportWeight> PostSetReportWeight(ulong xuid)
+        {
+            var path = new Uri(this.baseUri, $"{TitlePath}/player/{xuid}/reportWeight");
+
+            return await ServiceClient.SendRequestAsync<UserReportWeight>(HttpMethod.Post, path, this.authKey, Version, headers: this.headers).ConfigureAwait(false);
+        }
+    }
+}
---
+++ packages/StewardApi/StewardApiTest/Integration/Woodstock/V2/Player/ReportWeightController/ReportWeightControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Turn10.LiveOps.StewardTest.Utilities.TestingClient;
+
+namespace Turn10.LiveOps.StewardTest.Integration.Woodstock.V2
+{
+    [TestClass]
+    public sealed class ReportWeightControllerTests : WoodstockControllerTestsBase
+    {
+        private static ReportWeightControllerTestingClient stewardClient;
+        private static ReportWeightControllerTestingClient unauthedClient;
+
+        [ClassInitialize]
+        public static async Task Setup(TestContext testContext)
+        {
+            await PrepareAuthAsync(testContext);
+
+            stewardClient = new ReportWeightControllerTestingClient(new Uri(endpoint), authKey);
+            unauthedClient = new ReportWeightControllerTestingClient(new Uri(endpoint), TestConstants.InvalidAuthKey);
+
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetReportWeight()
+        {
+            try
+            {
+                var response = await stewardClient.GetReportWeight(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.IsNotNull(response);
+            }
+            catch (ServiceException ex)
+            {
+                Assert.Fail(ex.ResponseBody);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetReportWeight_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.GetReportWeight(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.InternalServerError, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task GetReportWeight_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.GetReportWeight(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostSetReportWeight_InvalidXuid()
+        {
+            try
+            {
+                var response = await stewardClient.PostSetReportWeight(TestConstants.InvalidXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.UnsupportedMediaType, ex.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        [IntegrationTest]
+        public async Task PostSetReportWeight_InvalidAuth()
+        {
+            try
+            {
+                var response = await unauthedClient.PostSetReportWeight(TestConstants.TestAccountXuid).ConfigureAwait(false);
+                Assert.Fail();
+            }
+            catch (ServiceException ex)
+            {
+                Assert.AreEqual(HttpStatusCode.Unauthorized, ex.StatusCode);
+            }
+        }
+    }
+}
